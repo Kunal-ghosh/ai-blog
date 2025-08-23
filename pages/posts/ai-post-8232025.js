2356c8715e13bca7f75478063caae2daf7481747
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-23 22:36:59 +0530

    post3

---
+++ pages/posts/ai-post-8232025.js
@@ -0,0 +1,8 @@
+export default function ai_post_8232025() {
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>AI Post 8/23/2025</h1>
+        <p>This is an automatically generated AI blog post on Sat Aug 23 2025. 🚀</p>
+      </div>
+    );
+  }
