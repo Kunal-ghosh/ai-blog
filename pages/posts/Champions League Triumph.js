632d5ae5550cee00ed9074ae916295cf8dd960b3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-24 17:09:41 +0530

    test1

---
+++ pages/posts/Champions League Triumph.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Post = () => (
+  <article style={{ padding: '2rem' }}>
+    <h1>AI Post 24-08-2025 17-09-03</h1>
+    <p>/s Champions League Triumph /s  
+/p Real Madrid clinched their 15th UEFA Champions League title, defeating Borussia Dortmund 2-0 at Wembley. Vinícius Jr. sealed victory with a second-half strike, while goalkeeper Thibaut Courtois starred with crucial saves. /p</p>
+  </article>
+);
+
+export default Post;
