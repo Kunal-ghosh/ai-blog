49c342538c60e67a6f6d569d95258ad4597999a9
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-25 14:51:29 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/untitled.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>untitled</h1>
+            <p>NASA gears up for the Artemis II mission, aiming to send four astronauts around the Moon in late 2024. The prelaunch tests and crew training mark a pivotal step toward sustainable lunar exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
