883e0d389e85eb701c7cf94b15c258d017b97ef1
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-25 19:36:07 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/NASA's Artemis II.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Artemis II</h1>
+            <p>NASA unveiled the Artemis II crew, targeting a 2024 lunar flyby mission. This historic voyage will pave the way for future human exploration of the Moon and Mars.</p>
+          </article>
+        );
+
+        export default Post;
+        
