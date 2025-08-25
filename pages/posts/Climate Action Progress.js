0bf75f533b134b13283c9627ac94a9b5fd954b12
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-25 15:50:53 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Climate Action Progress.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Action Progress</h1>
+            <p>Global leaders commit to tripling renewable energy by 2030 at UN summit, intensifying efforts to curb emissions amid worsening climate emergencies.</p>
+          </article>
+        );
+
+        export default Post;
+        
