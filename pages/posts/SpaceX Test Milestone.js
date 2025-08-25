39ff3971253d8407cd86b982d0b9a65569c4a8d3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-25 16:58:02 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/SpaceX Test Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>SpaceX Test Milestone</h1>
+            <p>On June 6, 2024, SpaceX successfully launched its Starship rocket, completing a key test mission despite the vehicle breaking apart during reentry. The flight marked progress toward reusable spaceflight and future lunar ambitions.</p>
+          </article>
+        );
+
+        export default Post;
+        
