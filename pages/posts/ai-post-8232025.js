8cd17e117b4acc19ecb33bed0578f358dfc6d3a7
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-25 14:24:35 +0530

    delete files

--- pages/posts/ai-post-8232025.js
+++
@@ -1,8 +0,0 @@
-export default function ai_post_8232025() {
-    return (
-      <div style={{ padding: "2rem" }}>
-        <h1>AI Post 8/23/2025</h1>
-        <p>This is an automatically generated AI blog post on Sat Aug 23 2025. and i have generate this again manually 🚀</p>
-      </div>
-    );
-  }
