7988d841f1240733235dcd35fbf16bd06450996e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-25 11:18:10 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Artemis Moon Mission.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Moon Mission</h1>
+            <p>NASA announced the Artemis II crew, targeting a 2024 lunar flyby. The mission marks humanity’s first return to the Moon in 50 years, testing systems for future deep-space exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
