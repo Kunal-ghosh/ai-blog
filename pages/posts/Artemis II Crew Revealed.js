a7e48cf12a1b7032a4ba7c1edc0015414014f0e2
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-26 10:54:00 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Artemis II Crew Revealed.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Crew Revealed</h1>
+            <p>NASA announced the four astronauts selected for the 2024 Artemis II mission, marking humanity’s first lunar flyby in 50 years. The crew, including the first woman and person of color on a Moon mission, will test systems for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
