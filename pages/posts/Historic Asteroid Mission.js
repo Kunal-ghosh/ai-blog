1b8834c98ccfba268315665ecd37ddade140405b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-26 18:36:31 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Historic Asteroid Mission.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Historic Asteroid Mission</h1>
+            <p>NASA's OSIRIS-REx spacecraft triumphantly delivered asteroid Bennu samples to Earth, unlocking secrets of our solar system's origins. Researchers celebrate the milestone, foreseeing breakthroughs in understanding planetary formation and organic materials.</p>
+          </article>
+        );
+
+        export default Post;
+        
