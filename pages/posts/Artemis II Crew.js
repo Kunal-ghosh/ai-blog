905d47b0f457aeabefd4cf39d81dbbdc4b246e1c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-26 11:56:28 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Artemis II Crew.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Crew</h1>
+            <p>NASA announced the Artemis II crew, finalizing preparations for humanity’s first lunar mission in 50 years. The mission, targeting a 2024 launch, aims to orbit the Moon, paving the way for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
