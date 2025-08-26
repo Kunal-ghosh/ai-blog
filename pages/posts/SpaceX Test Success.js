7315a948d2032435a294b4fdd16eeb379786324e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-26 17:11:27 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/SpaceX Test Success.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>SpaceX Test Success</h1>
+            <p>SpaceX's Starship completed its fourth test flight, achieving a successful splashdown in the Indian Ocean. The milestone advances reusable rocket tech, edging closer to Mars colonization goals.</p>
+          </article>
+        );
+
+        export default Post;
+        
