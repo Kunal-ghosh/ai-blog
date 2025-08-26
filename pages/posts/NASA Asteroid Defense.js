3e5d461921403876302b38850b188a42fb4da0c0
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-26 20:36:23 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/NASA Asteroid Defense.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Asteroid Defense</h1>
+            <p>NASA's DART mission successfully altered an asteroid's orbit, marking humanity's first test of planetary defense. The collision with Dimorphos demonstrated potential to deflect space threats, advancing Earth-protection strategies.</p>
+          </article>
+        );
+
+        export default Post;
+        
