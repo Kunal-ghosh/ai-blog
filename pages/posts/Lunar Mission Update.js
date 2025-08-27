9ebbd9bef0696dd4c2e5b30b7362385c0be901ea
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 18:34:37 +0000

    Add auto-generated post [skip ci]

--- pages/posts/Lunar Mission Update.js
+++ pages/posts/Lunar Mission Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Lunar Mission Update</h1>
-            <p>NASA's Artemis II crew completed final simulator training, preparing for a 2025 manned lunar orbit. The mission aims to pave the way for sustainable Moon exploration by late decade.</p>
+            <p>NASA announces Artemis II crew, targeting a 2024 lunar flyby. The mission aims to pave the way for future human Mars exploration.</p>
           </article>
         );
 
