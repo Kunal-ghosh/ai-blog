cb22613073c9ab543d74bbe3cd08f679dddd7ed6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 19:35:08 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Starliner Return Delayed.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Starliner Return Delayed</h1>
+            <p>NASA and Boeing delay Starliner's ISS return to June 22 for additional technical reviews, prioritizing astronaut safety amid ongoing thruster and helium leak assessments. The mission, crucial for future crewed spaceflight certifications, remains under close scrutiny.</p>
+          </article>
+        );
+
+        export default Post;
+        
