48f62710cc547ba008ea92fb90cf554db8a4f973
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 05:25:58 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Japan Earthquake Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Japan Earthquake Update</h1>
+            <p>A 6.6 magnitude earthquake struck Hokkaido, Japan, triggering landslides and power outages. Rescue teams are mobilizing as aftershocks hinder recovery efforts.</p>
+          </article>
+        );
+
+        export default Post;
+        
