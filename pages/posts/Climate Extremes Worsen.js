bf421505dace96ad0f021c6e461db922b519abba
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 16:33:59 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Climate Extremes Worsen.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Extremes Worsen</h1>
+            <p>Deadly heatwaves scorch India, hitting 50°C in regions, while Brazil battles historic floods displacing thousands, underscoring escalating climate crises.</p>
+          </article>
+        );
+
+        export default Post;
+        
