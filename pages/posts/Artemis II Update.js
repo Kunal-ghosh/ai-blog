0687a290b33ca25187429c763d2d1056c39feb7e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 12:38:29 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Artemis II Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Update</h1>
+            <p>NASA unveiled the Artemis II crew, targeting a 2025 lunar flyby. This first crewed mission since 1972 aims to test systems for future Moon and Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
