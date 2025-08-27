c5137e416888e75e5b14c0c53aeb46827fdd9eb6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 15:14:05 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Middle East Escalation.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Middle East Escalation</h1>
+            <p>Recent clashes between Israel and Hamas intensified, prompting Egypt to mediate ceasefire talks. Rising casualties and humanitarian crises spur global calls for immediate de-escalation and aid.</p>
+          </article>
+        );
+
+        export default Post;
+        
