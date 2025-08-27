28db4aa684751e1f956daf48760dcec7adba49c9
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 19:48:46 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Asteroid's Close Approach.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Asteroid's Close Approach</h1>
+            <p>A stadium-sized asteroid, 2024 MK, safely passed Earth on June 29, 2024, within 180,000 miles—closer than the Moon. NASA confirmed no impact risk, highlighting planetary defense tracking efforts.</p>
+          </article>
+        );
+
+        export default Post;
+        
