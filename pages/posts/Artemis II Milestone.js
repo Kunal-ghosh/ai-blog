f805660c9bbba3a462da1ab78a6e44772bf89ed4
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 19:20:15 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Artemis II Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Milestone</h1>
+            <p>NASA announces Artemis II mission for 2025, aiming to send astronauts around the Moon for the first time in 50 years. The crewed lunar flyby paves the way for future sustainable exploration and Mars missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
