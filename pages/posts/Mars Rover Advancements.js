43ea90b868e826b598babeaa027afdee5dedfdbc
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-27 15:34:12 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Mars Rover Advancements.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Rover Advancements</h1>
+            <p>ESA announces 2028 launch for ExoMars Rosalind Franklin rover, aiming to explore Oxia Planum for signs of ancient microbial life. The mission will deploy advanced drilling tech to analyze Martian soil deeper than ever before.</p>
+          </article>
+        );
+
+        export default Post;
+        
