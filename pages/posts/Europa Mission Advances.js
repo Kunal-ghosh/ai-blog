d8871d009f8916f431af3b641d8a49c446ba62f4
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-28 18:40:01 +0000

    Add auto-generated post

---
+++ pages/posts/Europa Mission Advances.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Europa Mission Advances</h1>
+            <p>NASA's Europa Clipper spacecraft, launching October 2024, will study Jupiter’s moon Europa to analyze its icy shell and hidden ocean, potentially unveiling clues for extraterrestrial life.</p>
+          </article>
+        );
+
+        export default Post;
+        
