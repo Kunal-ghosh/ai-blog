6eefcfa7cac7c97510e7a2d3da11e9e342d29d0b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-28 19:12:29 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Rover Launch.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Rover Launch</h1>
+            <p>NASA's VIPER rover prepares to explore the Moon's south pole in 2024, aiming to map water ice. The mission could revolutionize future lunar colonization and deep-space exploration efforts.</p>
+          </article>
+        );
+
+        export default Post;
+        
