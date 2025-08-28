c4366120202fce7265dadda81bb3fdd531c59035
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-28 17:21:54 +0000

    Add auto-generated post [skip ci]

---
+++ pages/posts/Europa Clipper Launch.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Europa Clipper Launch</h1>
+            <p>NASA's Europa Clipper mission, set to launch in October 2024, aims to explore Jupiter's moon Europa. The spacecraft will study its icy crust and subsurface ocean to assess potential conditions for life beyond Earth.</p>
+          </article>
+        );
+
+        export default Post;
+        
