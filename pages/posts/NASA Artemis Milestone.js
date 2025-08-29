919ee26418b89e887f05cb71e6d307cf83f91c6c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 22:39:02 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Artemis Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Artemis Milestone</h1>
+            <p>NASA announces Artemis II crew, finalizing preparations for a 2024 lunar flyby—marking humanity's first manned Moon mission in over 50 years. The mission aims to pave the way for sustainable lunar exploration and future Mars expeditions.</p>
+          </article>
+        );
+
+        export default Post;
+        
