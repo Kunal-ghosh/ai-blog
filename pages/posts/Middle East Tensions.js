4a67a64cc429d2880f60cbf7191c4a05d3f1dff3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 08:57:03 +0000

    Add auto-generated post

---
+++ pages/posts/Middle East Tensions.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Middle East Tensions</h1>
+            <p>Recent escalations in the Israel-Palestine conflict have sparked global concern, with intensified airstrikes and clashes causing civilian casualties. International leaders urge de-escalation as humanitarian crises deepen in Gaza.</p>
+          </article>
+        );
+
+        export default Post;
+        
