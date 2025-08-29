3360f30df2b9af1c6bf4b89fd343a11c185e3c0e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 15:38:30 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Landing Success.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Landing Success</h1>
+            <p>Intuitive Machines' Odysseus made history as the first private spacecraft to land on the moon, revitalizing lunar exploration. The mission, supported by NASA, aims to pave the way for future crewed missions and sustainable lunar research.</p>
+          </article>
+        );
+
+        export default Post;
+        
