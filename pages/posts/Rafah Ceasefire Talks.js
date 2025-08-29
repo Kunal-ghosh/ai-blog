d6fca08d8203a0e67652b9b8dd7392265c280e6c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 18:23:16 +0000

    Add auto-generated post

---
+++ pages/posts/Rafah Ceasefire Talks.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Rafah Ceasefire Talks</h1>
+            <p>Escalating tensions in Gaza’s Rafah prompt renewed ceasefire negotiations between Israel and Hamas, amid urgent humanitarian appeals. Egypt and the US mediate as global calls for de-escalation intensify.</p>
+          </article>
+        );
+
+        export default Post;
+        
