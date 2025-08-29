4b98b03ca027e01a5066df7bc589177ab58c42ea
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 20:18:11 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Moon Mission.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Moon Mission</h1>
+            <p>NASA announces Artemis III's 2026 lunar landing target, focusing on exploring the Moon’s South Pole with SpaceX’s Starship. The mission aims to advance sustainable human presence in deep space.</p>
+          </article>
+        );
+
+        export default Post;
+        
