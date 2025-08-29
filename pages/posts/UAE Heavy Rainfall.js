4359b315ad9b879b22621525261912d51e014424
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 08:28:06 +0000

    Add auto-generated post

---
+++ pages/posts/UAE Heavy Rainfall.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>UAE Heavy Rainfall</h1>
+            <p>Unprecedented torrential rains flooded parts of the UAE, submerging Dubai's streets and halting flights. The extreme weather, linked to climate shifts, caused widespread disruption and recovery efforts.</p>
+          </article>
+        );
+
+        export default Post;
+        
