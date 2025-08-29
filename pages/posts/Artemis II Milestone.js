652840553dc04ae4ea7061464bfcee0e310777a6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 16:45:17 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis II Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Milestone</h1>
+            <p>NASA's Artemis II mission advances, with astronauts completing intensive training for the first crewed lunar flyby since 1972. Scheduled for 2024, the mission aims to pave the way for sustained human exploration of the Moon and Mars.</p>
+          </article>
+        );
+
+        export default Post;
+        
