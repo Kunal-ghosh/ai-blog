c2e94a6cc9133e63c7650ea5668eede7b02cfc81
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 06:48:47 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Exploration Updates.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Exploration Updates</h1>
+            <p>NASA unveils Artemis II crew training progress, targeting a 2025 lunar flyby mission. The team, including international astronauts, aims to pave the way for sustainable Moon exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
