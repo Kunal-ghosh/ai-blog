26aa7e8f856a8abd0c73ce6b08f955c591a4cf6f
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 18:45:13 +0000

    Add auto-generated post

---
+++ pages/posts/UK Election Results.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>UK Election Results</h1>
+            <p>Labour Party secures landslide victory in UK general election, ending 14 years of Conservative rule. Keir Starmer to become Prime Minister amid promises of economic renewal and public service reforms.</p>
+          </article>
+        );
+
+        export default Post;
+        
