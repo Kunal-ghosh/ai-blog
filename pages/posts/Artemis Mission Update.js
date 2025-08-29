71655ff489320d48cb460e462b8dea5a5942eb3c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 09:39:57 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Mission Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Mission Update</h1>
+            <p>NASA accelerates Artemis III, targeting a 2025 lunar landing with SpaceX and ESA collaboration, reigniting global interest in sustainable Moon exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
