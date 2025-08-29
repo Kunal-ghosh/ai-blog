9b0d0234740ab9beafb43747b2bfc081e6877b14
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 21:15:11 +0000

    Add auto-generated post

---
+++ pages/posts/Greece Wildfires Intensify.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Greece Wildfires Intensify</h1>
+            <p>Raging wildfires fueled by extreme heatwaves have forced mass evacuations in southern Greece, destroying homes and forests. Authorities warn of escalating climate risks as firefighters battle blazes amid record temperatures.</p>
+          </article>
+        );
+
+        export default Post;
+        
