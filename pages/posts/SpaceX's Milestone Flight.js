aff755ff8461e3f3ed50d210b9383b5b3e2b2673
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-29 03:26:36 +0000

    Add auto-generated post

---
+++ pages/posts/SpaceX's Milestone Flight.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>SpaceX's Milestone Flight</h1>
+            <p>SpaceX successfully launched and landed its Starship rocket in a fourth test flight, advancing reusable spacecraft development. The mission brings humanity closer to future Mars colonization efforts.</p>
+          </article>
+        );
+
+        export default Post;
+        
