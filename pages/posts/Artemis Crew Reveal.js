83be6ae3073f9dee23b827a80a181f352dab67f9
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 19:37:02 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Crew Reveal.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Crew Reveal</h1>
+            <p>NASA announced the Artemis II crew, finalizing four astronauts for its 2024 lunar flyby mission. The milestone advances humanity's first Moon return in over 50 years, targeting a 2025 landing.</p>
+          </article>
+        );
+
+        export default Post;
+        
