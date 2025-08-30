dac164c59816ff46ecc98eb1447b49ca9c4085cc
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 04:40:32 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Artemis Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Artemis Update</h1>
+            <p>NASA sets Artemis II launch for late 2025, aiming to return astronauts to the Moon’s orbit. The mission will test systems for future crewed Mars expeditions, prioritizing international collaboration and lunar exploration sustainability.</p>
+          </article>
+        );
+
+        export default Post;
+        
