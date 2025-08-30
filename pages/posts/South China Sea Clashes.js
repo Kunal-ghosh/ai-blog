e88df2f11b4623c63b6b4fc8df63b7aa2149855b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 16:42:41 +0000

    Add auto-generated post

---
+++ pages/posts/South China Sea Clashes.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>South China Sea Clashes</h1>
+            <p>Recent confrontations between Chinese and Philippine vessels near the Second Thomas Shoal escalated tensions. Manila condemned China’s use of water cannons, calling it "dangerous aggression," while Beijing reaffirmed its territorial claims.</p>
+          </article>
+        );
+
+        export default Post;
+        
