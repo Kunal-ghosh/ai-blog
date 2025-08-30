d254796d86bb52f87f74386bbf98e65b15e40051
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 01:15:10 +0000

    Add auto-generated post

---
+++ pages/posts/Kharkiv Market Strike.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Kharkiv Market Strike</h1>
+            <p>Russian missile strike hits Kharkiv market, killing at least 10 and injuring dozens amid escalating Ukraine-Russia conflict. International leaders condemn attack, urging renewed diplomatic efforts to halt civilian toll.</p>
+          </article>
+        );
+
+        export default Post;
+        
