c53649019e9ece548f20802f14f5e1306d217a6d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 23:36:55 +0000

    Add auto-generated post

---
+++ pages/posts/Euro 2024 Final.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Euro 2024 Final</h1>
+            <p>Spain clinched victory against England in a thrilling 2-1 match, securing their fourth European Championship title. Historic goals by Nico Williams and Mikel Oyarzabal sealed the win amid intense Berlin crowds.</p>
+          </article>
+        );
+
+        export default Post;
+        
