fc2c16ee369e69969f8ce46d9f37259389821029
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 17:36:39 +0000

    Add auto-generated post

---
+++ pages/posts/Moon Mission Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Moon Mission Update</h1>
+            <p>NASA's Artemis II crewed lunar flyby prepares for 2025 launch, marking humanity's first return to the Moon in 50 years. The mission aims to test systems for future sustainable lunar exploration and Mars ambitions.</p>
+          </article>
+        );
+
+        export default Post;
+        
