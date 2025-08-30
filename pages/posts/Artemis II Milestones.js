c3d9a5c55a190be8ad2717d1c9075c22362bcf4f
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 03:28:11 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis II Milestones.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Milestones</h1>
+            <p>NASA advances Artemis II, targeting a 2025 lunar mission. The crew completes critical training, paving the way for humankind's return to the Moon amid global anticipation.</p>
+          </article>
+        );
+
+        export default Post;
+        
