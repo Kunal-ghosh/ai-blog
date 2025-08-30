1db83821066d02c0bc1f59f1deb641b28ffe22e6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 03:47:11 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis II Crew.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Crew</h1>
+            <p>NASA unveils crew for 2024 Artemis II mission, marking the first lunar flyby in 50 years. The diverse team aims to pave the way for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
