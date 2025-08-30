c8c048e97b546c40fd56b812be330f57e4d2da73
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 16:18:56 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Rover Collaboration.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Rover Collaboration</h1>
+            <p>NASA and ESA's 2024 ExoMars mission successfully deployed the *Rosalind Franklin* rover, aiming to search for ancient microbial life. The rover will drill deeper than ever before, analyzing Martian soil for biosignatures.</p>
+          </article>
+        );
+
+        export default Post;
+        
