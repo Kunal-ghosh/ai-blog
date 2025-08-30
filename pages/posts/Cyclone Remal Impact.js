49d75d83c4b86cff8ded2e1a4465ead0ce366199
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 21:14:05 +0000

    Add auto-generated post

---
+++ pages/posts/Cyclone Remal Impact.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Cyclone Remal Impact</h1>
+            <p>Cyclone Remal devastated coastal regions of Bangladesh and India, triggering massive floods and displacing over 500,000 people. Authorities scramble relief efforts amid widespread infrastructure damage and rising humanitarian concerns.</p>
+          </article>
+        );
+
+        export default Post;
+        
