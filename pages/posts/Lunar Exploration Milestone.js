6e88cc4a91ccf87aceb09b20914f94f645f330f4
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-30 14:37:59 +0000

    Add auto-generated post

--- pages/posts/Lunar Exploration Milestone.js
+++ pages/posts/Lunar Exploration Milestone.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Lunar Exploration Milestone</h1>
-            <p>NASA's Artemis II mission, set for a 2025 lunar flyby, announced its diverse astronaut crew, marking humanity's first moon voyage in 50 years. The mission aims to pave the way for future lunar exploration and Mars missions.</p>
+            <p>NASA announces Artemis II, targeting a 2025 crewed lunar flyby. The mission aims to pave the way for sustainable Moon exploration, with four astronauts set to orbit Earth’s satellite for the first time in 50 years.</p>
           </article>
         );
 
