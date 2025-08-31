8aa0a74f545c34429d2a8916b8f18098719357a1
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-31 11:36:53 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Far Side Mission.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Far Side Mission</h1>
+            <p>China's Chang'e-6 probe successfully landed on the Moon’s far side, marking a historic step in collecting lunar samples. The mission aims to unlock mysteries about the Moon’s formation and expand space exploration capabilities.</p>
+          </article>
+        );
+
+        export default Post;
+        
