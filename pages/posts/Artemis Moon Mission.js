33cf8f94f2384c45e58c4e94d9212e74c493b395
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-31 21:14:42 +0000

    Add auto-generated post

--- pages/posts/Artemis Moon Mission.js
+++ pages/posts/Artemis Moon Mission.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis Moon Mission</h1>
-            <p>NASA's Artemis II mission, a collaboration with SpaceX, launched the first crewed lunar flyby in 50 years, aiming to pave the way for sustainable lunar exploration and future Mars missions. The four astronauts aboard will orbit the Moon, testing systems for long-duration deep-space travel.</p>
+            <p>NASA announces Artemis III plans, targeting a 2026 crewed lunar landing to advance deep-space exploration and sustain future Mars missions.</p>
           </article>
         );
 
