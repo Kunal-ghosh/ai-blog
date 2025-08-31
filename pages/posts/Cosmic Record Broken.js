16b67d09f93d1fe55d8cb7197574ad21b0e908ad
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-31 21:36:23 +0000

    Add auto-generated post

---
+++ pages/posts/Cosmic Record Broken.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Cosmic Record Broken</h1>
+            <p>The James Webb Space Telescope recently identified the oldest-known galaxies, formed just 290 million years after the Big Bang. This discovery reshapes understanding of early cosmic evolution and star formation.</p>
+          </article>
+        );
+
+        export default Post;
+        
