cb2b9f8c680f59699973bf106080b791848695e0
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-31 04:41:35 +0000

    Add auto-generated post

--- pages/posts/NASA Artemis Update.js
+++ pages/posts/NASA Artemis Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>NASA Artemis Update</h1>
-            <p>NASA sets Artemis II launch for late 2025, aiming to return astronauts to the Moon’s orbit. The mission will test systems for future crewed Mars expeditions, prioritizing international collaboration and lunar exploration sustainability.</p>
+            <p>NASA unveils Artemis II crew, targeting a 2024 lunar flyby mission. The milestone paves the way for future human exploration of Mars.</p>
           </article>
         );
 
