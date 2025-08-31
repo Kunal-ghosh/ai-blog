7e297878aea93e80ffc3f3980c501c027b36866c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-31 05:16:00 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Mission Delay.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Mission Delay</h1>
+            <p>NASA delays Artemis II crewed mission to 2025, citing spacecraft safety and technical challenges. The postponement aims to address issues ahead of future lunar exploration goals.</p>
+          </article>
+        );
+
+        export default Post;
+        
