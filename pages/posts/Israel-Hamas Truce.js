bfd0b3b14ffefea87c757890ba7f1ea65032b54c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-31 09:15:40 +0000

    Add auto-generated post

---
+++ pages/posts/Israel-Hamas Truce.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Israel-Hamas Truce</h1>
+            <p>Egypt brokers temporary Israel-Hamas ceasefire, easing Gaza conflict; humanitarian aid flows in amid global calls for lasting peace.</p>
+          </article>
+        );
+
+        export default Post;
+        
