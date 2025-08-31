9160b63fbfa4cf125bd81e24b6c625dff7f63c2f
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-31 13:21:54 +0000

    Add auto-generated post

---
+++ pages/posts/Climate Protest Escalation.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Protest Escalation</h1>
+            <p>Climate activists with Extinction Rebellion and Just Stop Oil hurled soup at the Mona Lisa's protective glass in Paris' Louvre, demanding immediate action to end fossil fuels amid rising global temperature concerns.</p>
+          </article>
+        );
+
+        export default Post;
+        
