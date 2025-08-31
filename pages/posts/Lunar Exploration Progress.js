fdd34151724cac50c3143aca6a5b5ad947700348
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-08-31 14:14:21 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Exploration Progress.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Exploration Progress</h1>
+            <p>NASA recently announced advancements in the Artemis program, confirming plans for the Lunar Gateway station with international partners. The mission aims to return astronauts to the Moon by 2026, fostering sustainable deep-space exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
