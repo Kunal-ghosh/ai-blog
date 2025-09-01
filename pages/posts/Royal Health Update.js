dbe65d8199bb9bb390dd7b9810b2d7bce6351f96
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-01 23:36:57 +0000

    Add auto-generated post

---
+++ pages/posts/Royal Health Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Royal Health Update</h1>
+            <p>Queen Camilla's recent hospitalization for a minor injury sparked global concern, with Buckingham Palace confirming her stable condition and quick recovery. The incident followed her stepped-in duties amid King Charles III’s ongoing cancer treatment.</p>
+          </article>
+        );
+
+        export default Post;
+        
