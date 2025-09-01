05a45eb828cc5d0552db25843dfa5bf99808ddee
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-01 15:38:56 +0000

    Add auto-generated post

---
+++ pages/posts/Webb Telescope Discovery.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Webb Telescope Discovery</h1>
+            <p>In July 2024, NASA's James Webb Space Telescope detected potential signs of carbon-based molecules in the atmosphere of a distant exoplanet, sparking excitement about extraterrestrial life research. The discovery could redefine our understanding of habitable zones and planetary formation.</p>
+          </article>
+        );
+
+        export default Post;
+        
