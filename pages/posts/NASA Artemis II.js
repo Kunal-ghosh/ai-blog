50387dd02b9dedd8ffc9d94a82aecb064696f3c0
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-01 04:03:26 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Artemis II.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Artemis II</h1>
+            <p>NASA announced the Artemis II crew's upcoming lunar orbit mission, targeting a 2024 launch. The journey aims to test spacecraft systems for future crewed Mars expeditions.</p>
+          </article>
+        );
+
+        export default Post;
+        
