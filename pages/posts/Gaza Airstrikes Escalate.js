d10284e4d4807132f0a0b585017f6a726bfb2a4b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-02 17:15:04 +0000

    Add auto-generated post

---
+++ pages/posts/Gaza Airstrikes Escalate.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Gaza Airstrikes Escalate</h1>
+            <p>Israeli airstrikes intensify in Gaza, killing over 100 in 24 hours. International bodies urge ceasefire as humanitarian crises deepen.</p>
+          </article>
+        );
+
+        export default Post;
+        
