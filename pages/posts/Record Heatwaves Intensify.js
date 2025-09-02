b5160f31bad24cf3a5d2cd3d80a9c427f8a408e0
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-02 16:21:23 +0000

    Add auto-generated post

---
+++ pages/posts/Record Heatwaves Intensify.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Record Heatwaves Intensify</h1>
+            <p>Europe and Asia face unprecedented temperatures as climate agencies confirm July 2023 as the hottest month ever recorded, urging global action to curb emissions.</p>
+          </article>
+        );
+
+        export default Post;
+        
