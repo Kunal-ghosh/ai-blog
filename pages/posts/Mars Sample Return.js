c63c30db7f64068fb902ac5a8d5c3f4ac0d7a71d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-02 12:36:47 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Sample Return.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Sample Return</h1>
+            <p>NASA and ESA collaborate on Mars Sample Return mission, with Perseverance rover collecting Martian rocks. The samples aim to reveal clues about ancient microbial life, marking humanity's first attempt to retrieve material from another planet.</p>
+          </article>
+        );
+
+        export default Post;
+        
