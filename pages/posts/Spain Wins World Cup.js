fe8349f44267e6ee59f1ac103352259e01a22651
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-02 18:22:44 +0000

    Add auto-generated post

---
+++ pages/posts/Spain Wins World Cup.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Spain Wins World Cup</h1>
+            <p>Spain clinched their first FIFA Women's World Cup title, defeating England 1-0 in a historic final. A record-breaking crowd in Sydney celebrated as Olga Carmona's goal secured victory, marking a milestone for women's football.</p>
+          </article>
+        );
+
+        export default Post;
+        
