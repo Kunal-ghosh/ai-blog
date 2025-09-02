13090887c045ad4f6e559e814f874c6405a2881e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-02 23:36:29 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Europa Exploration.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Europa Exploration</h1>
+            <p>NASA's Europa Clipper spacecraft launched October 2023 to study Jupiter’s icy moon, Europa, for potential habitable conditions. The mission aims to analyze its subsurface ocean and chemical composition, advancing the search for extraterrestrial life.</p>
+          </article>
+        );
+
+        export default Post;
+        
