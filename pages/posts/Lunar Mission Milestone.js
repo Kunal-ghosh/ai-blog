fab4f2d0c0320300ff05cdecef0ca2ed17702cdf
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-02 15:18:49 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Mission Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Mission Milestone</h1>
+            <p>NASA’s Artemis II mission, launching in 2024, will send the first woman and person of color on a lunar flyby. This crewed test flight paves the way for sustainable Moon exploration by 2030.</p>
+          </article>
+        );
+
+        export default Post;
+        
