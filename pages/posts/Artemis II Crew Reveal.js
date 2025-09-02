0d1204daa0cd7ea4931467d006e1931d1e7a8ac9
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-02 22:14:37 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis II Crew Reveal.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Crew Reveal</h1>
+            <p>NASA announced the diverse crew for Artemis II, the first manned lunar mission in 50 years. Slated for 2024, the team aims to orbit the Moon, paving the way for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
