52e63a6cc2678cace50ce7b9b24ad0cdafc99864
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-02 20:37:58 +0000

    Add auto-generated post

---
+++ pages/posts/Japan Moon Landing.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Japan Moon Landing</h1>
+            <p>Japan's SLIM spacecraft made history by landing on the Moon on January 19, 2024, becoming the nation’s first successful lunar mission. The probe faced power issues due to tilted solar panels but transmitted crucial data before shutdown.</p>
+          </article>
+        );
+
+        export default Post;
+        
