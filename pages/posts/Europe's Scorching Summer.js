192857c41cada8f14281dc0f007287aa78483101
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-03 16:21:07 +0000

    Add auto-generated post

---
+++ pages/posts/Europe's Scorching Summer.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Europe's Scorching Summer</h1>
+            <p>Sweden recorded its highest-ever July temperature this week, surpassing 38°C, while wildfires forced emergency evacuations in Spain amid intensifying heatwaves across Europe.</p>
+          </article>
+        );
+
+        export default Post;
+        
