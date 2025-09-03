65e1ecbceeabe627bd3fc163b7fec4cf4454e7e3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-03 10:40:49 +0000

    Add auto-generated post

---
+++ pages/posts/Space Exploration Breakthrough.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Space Exploration Breakthrough</h1>
+            <p>NASA's Europa Clipper launches October 2024 to study Jupiter's icy moon Europa, aiming to uncover potential habitats for life beneath its frozen crust. The mission could revolutionize understanding of ocean worlds in our solar system.</p>
+          </article>
+        );
+
+        export default Post;
+        
