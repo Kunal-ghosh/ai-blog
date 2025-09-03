34d2b31bdc2e5d75d523c7bed5456b4717229908
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-03 05:39:02 +0000

    Add auto-generated post

---
+++ pages/posts/Olympics Water Crisis.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Olympics Water Crisis</h1>
+            <p>Recent tests revealed elevated E. coli levels in Paris' Seine River, raising concerns for marathon swimming events at the 2024 Olympics. Organizers are accelerating cleanup efforts to ensure athlete safety.</p>
+          </article>
+        );
+
+        export default Post;
+        
