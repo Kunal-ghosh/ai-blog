90f9a1f9359c3eb68155515b0175a19a6c2d8c23
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-03 22:38:04 +0000

    Add auto-generated post

---
+++ pages/posts/Turkey Boat Tragedy.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Turkey Boat Tragedy</h1>
+            <p>A dragon boat capsized in Turkey's Izmir province on June 6, 2024, claiming at least 14 lives. Rescue teams continue searching for survivors amid adverse weather conditions.</p>
+          </article>
+        );
+
+        export default Post;
+        
