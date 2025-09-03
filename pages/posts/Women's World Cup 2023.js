61a421c1fba7708946bd1760453fb267c62e46b3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-03 09:17:56 +0000

    Add auto-generated post

---
+++ pages/posts/Women's World Cup 2023.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Women's World Cup 2023</h1>
+            <p>Australia and New Zealand co-host the 2023 FIFA Women's World Cup, with record-breaking attendance as Australia secured a 1-0 victory against Ireland in the opener. Spain later dominated Costa Rica 3-0, sparking global excitement for women's football.</p>
+          </article>
+        );
+
+        export default Post;
+        
