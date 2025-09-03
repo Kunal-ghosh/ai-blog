258a86d506660e0a07cde58efb715f56d609acba
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-03 11:14:46 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Moon Launch.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Moon Launch</h1>
+            <p>NASA's Artemis I mission successfully concluded, splashing down in the Pacific after a 25-day journey around the Moon. The uncrewed test paves the way for future astronaut missions, aiming to return humans to lunar surfaces by 2025.</p>
+          </article>
+        );
+
+        export default Post;
+        
