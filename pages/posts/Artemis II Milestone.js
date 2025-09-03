9e001051b4c0f87aa0e1221039060f8500da7e80
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-03 07:37:24 +0000

    Add auto-generated post

--- pages/posts/Artemis II Milestone.js
+++ pages/posts/Artemis II Milestone.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Milestone</h1>
-            <p>NASA announces Artemis II crew for 2024 lunar flyby, aiming to propel humanity toward Mars exploration. The mission marks the first moon crew in 50 years, reigniting global lunar ambitions.</p>
+            <p>NASA recently unveiled the crew for Artemis II, marking humanity's first lunar mission in 50 years. The 2024 flight will test systems for future Mars exploration, reigniting global interest in deep-space travel.</p>
           </article>
         );
 
