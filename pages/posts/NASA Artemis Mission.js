a572267e61c97be666ac0bfa253c32bb67e3c8c3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-04 05:38:48 +0000

    Add auto-generated post

--- pages/posts/NASA Artemis Mission.js
+++ pages/posts/NASA Artemis Mission.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>NASA Artemis Mission</h1>
-            <p>NASA announces Artemis II crew, targeting a 2024 lunar flyby. The mission marks humanity's first return to the Moon since 1972, paving the way for future Mars exploration.</p>
+            <p>NASA announces Artemis II mission, targeting a 2025 crewed lunar landing. The spacecraft will orbit the Moon with four astronauts, marking humanity's first return since 1972.</p>
           </article>
         );
 
