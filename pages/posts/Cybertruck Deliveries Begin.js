624a6a5c2bbeab16a9e0c5dac3e46cbfcefaf118
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-04 11:36:43 +0000

    Add auto-generated post

---
+++ pages/posts/Cybertruck Deliveries Begin.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Cybertruck Deliveries Begin</h1>
+            <p>Tesla commenced first Cybertruck deliveries on November 30, unveiling its stainless-steel electric pickup with 0-60 mph in 2.6 seconds. The launch sparks debates on pricing and scalability amid mixed industry reactions to its futuristic design.</p>
+          </article>
+        );
+
+        export default Post;
+        
