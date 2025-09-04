8d155156bca3046883ff0e0e039c47176ab663f1
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-04 01:15:14 +0000

    Add auto-generated post

---
+++ pages/posts/Ancient Galaxy Discovery.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Ancient Galaxy Discovery</h1>
+            <p>James Webb Telescope reveals JADES-GS-z14-0, a 13.4-billion-year-old galaxy, reshaping understanding of early cosmic star formation. Its unexpected brightness challenges prior theories on universe's infancy.</p>
+          </article>
+        );
+
+        export default Post;
+        
