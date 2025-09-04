6b0802b0401f1e72746bc028a1ebe23740ab9ea3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-04 03:45:51 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis II Launch.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Launch</h1>
+            <p>NASA's Artemis II mission, set for November 2024, will send four astronauts on a lunar flyby, marking humanity's first return to the Moon in 50 years. The crewed test flight paves the way for future lunar exploration and sustained deep-space missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
