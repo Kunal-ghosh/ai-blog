b029b92d397c1805f0fd1faf84b6a4251553bc57
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-04 13:42:47 +0000

    Add auto-generated post

---
+++ pages/posts/SpaceX Milestone Achieved.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>SpaceX Milestone Achieved</h1>
+            <p>SpaceX successfully launched its Starship rocket on June 6, completing its fourth test flight with a breakthrough landing in the Indian Ocean. The prototypes survived re-entry burns, marking progress toward reusable spaceflight and future Mars missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
