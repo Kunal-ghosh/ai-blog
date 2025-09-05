73b4889c8d6c0f5cbe3ff6a1bef764a97aa040a3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-05 15:18:01 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Telescope Safe Mode.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Telescope Safe Mode</h1>
+            <p>NASA's Chandra X-ray Observatory entered safe mode on July 12, halting science operations. Engineers are investigating the cause while ensuring the spacecraft’s safety.</p>
+          </article>
+        );
+
+        export default Post;
+        
