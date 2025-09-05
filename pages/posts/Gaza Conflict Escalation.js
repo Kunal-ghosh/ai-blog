6f203b7e79810bbf8533a965e695ffba41776e57
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-05 06:47:57 +0000

    Add auto-generated post

---
+++ pages/posts/Gaza Conflict Escalation.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Gaza Conflict Escalation</h1>
+            <p>Renewed clashes in Gaza intensify humanitarian crises, with reports of civilian casualties and infrastructure damage. International mediators urge ceasefire as tensions strain diplomatic efforts.</p>
+          </article>
+        );
+
+        export default Post;
+        
