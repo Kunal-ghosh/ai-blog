3100f83c6ffecf1f759dec9dbe6e4c0c62476b03
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-05 07:37:14 +0000

    Add auto-generated post

---
+++ pages/posts/Antarctic Ice Crisis.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Antarctic Ice Crisis</h1>
+            <p>Scientists confirm a massive ice shelf collapse in Antarctica, revealing a 1,200-square-mile breach. Rising temperatures accelerate glacial melt, sparking urgent global calls to address climate-induced impacts on coastal regions.</p>
+          </article>
+        );
+
+        export default Post;
+        
