8ba4d7de3453f8168f6fba6c7f893995b0dd5a88
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-05 11:13:52 +0000

    Add auto-generated post

---
+++ pages/posts/Ukraine-Russia Conflict Escalates.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Ukraine-Russia Conflict Escalates</h1>
+            <p>A recent missile strike in Kharkiv, Ukraine, killed at least 12 civilians, drawing global condemnation. Ukraine reiterated calls for advanced air defense systems as Russia intensifies offensives ahead of winter.</p>
+          </article>
+        );
+
+        export default Post;
+        
