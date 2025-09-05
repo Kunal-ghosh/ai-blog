d53b832e7eff2f0661e0c50f483e4e1923f0c188
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-05 13:25:19 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Moon Mission Progress.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Moon Mission Progress</h1>
+            <p>NASA announces Artemis II crew assignments, advancing plans for a 2025 lunar orbit mission. The effort aims to establish sustainable lunar exploration with international partners.</p>
+          </article>
+        );
+
+        export default Post;
+        
