4935b55e7d79ba986ef89653a769198b5e941e86
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-06 02:38:38 +0000

    Add auto-generated post

--- pages/posts/NASA Artemis Update.js
+++ pages/posts/NASA Artemis Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>NASA Artemis Update</h1>
-            <p>NASA unveils Artemis II crew, targeting a 2024 lunar flyby mission. The milestone paves the way for future human exploration of Mars.</p>
+            <p>NASA announces Artemis II crew, targeting a 2024 lunar flyby mission to test systems for future Mars exploration. The diverse team includes astronauts from the U.S. and Canada, marking a milestone in international space collaboration.</p>
           </article>
         );
 
