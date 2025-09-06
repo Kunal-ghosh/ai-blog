98f8483b7e0d156d9f577a8171af716f20817a72
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-06 17:13:04 +0000

    Add auto-generated post

--- pages/posts/NASA Artemis II Crew.js
+++ pages/posts/NASA Artemis II Crew.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>NASA Artemis II Crew</h1>
-            <p>NASA announces international Artemis II crew, targeting a 2024 lunar flyby mission. The team includes astronauts from the U.S., Canada, and a historic first Canadian to orbit the Moon.</p>
+            <p>NASA announced the Artemis II crew, targeting a 2024 lunar flyby, marking humanity's first moon mission in 50 years. The diverse team aims to pave the way for future Mars exploration.</p>
           </article>
         );
 
