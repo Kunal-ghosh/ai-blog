af03b2072bc23ab098c35a0c5df7cf6123dba9d9
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-06 07:14:12 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's DART Success.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's DART Success</h1>
+            <p>NASA's DART spacecraft successfully altered an asteroid's orbit by crashing into Dimorphos, marking humanity's first test of planetary defense. The mission aims to develop techniques for deflecting potential Earth-threatening space objects.</p>
+          </article>
+        );
+
+        export default Post;
+        
