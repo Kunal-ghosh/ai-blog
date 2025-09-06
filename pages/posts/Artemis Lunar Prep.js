44872e8432b6ec9cf3319d5a4e8112a4ad463e77
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-06 05:37:45 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Lunar Prep.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Lunar Prep</h1>
+            <p>NASA astronauts recently completed a key simulation for the Artemis II mission, rehearsing Moon landing protocols in Houston. The exercise aimed to refine spacecraft systems and crew coordination ahead of the scheduled 2025 lunar orbit mission.</p>
+          </article>
+        );
+
+        export default Post;
+        
