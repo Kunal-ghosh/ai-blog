16399387f96a2be87a0430f991b9662c8cca365b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 23:14:24 +0000

    Add auto-generated post

---
+++ pages/posts/Space Mission Success.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Space Mission Success</h1>
+            <p>NASA and SpaceX's Crew-7 safely returned to Earth after a six-month ISS mission, advancing global collaboration in space exploration. The diverse crew conducted over 200 experiments, aiding future lunar and Mars missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
