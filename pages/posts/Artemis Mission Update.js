25755f10fc6bd9a239af587a7aec5971a526bb53
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 01:22:33 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Mission Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Mission Update</h1>
+            <p>NASA announced Artemis II, set for 2025, will send astronauts to lunar orbit for the first time in 50 years, paving the way for future Mars exploration amid global scientific collaboration.</p>
+          </article>
+        );
+
+        export default Post;
+        
