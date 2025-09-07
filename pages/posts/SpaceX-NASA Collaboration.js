ab2fd8fabe39de227f50a1f37999342dd64bba52
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 22:14:47 +0000

    Add auto-generated post

---
+++ pages/posts/SpaceX-NASA Collaboration.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>SpaceX-NASA Collaboration</h1>
+            <p>SpaceX's Crew-8 mission launched successfully, delivering astronauts to the ISS for a six-month scientific research expedition. The Falcon 9 rocket liftoff marks continued public-private space exploration advancements.</p>
+          </article>
+        );
+
+        export default Post;
+        
