019f7e2b45215c8669d7333522c56c8b35ac1e09
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 12:32:05 +0000

    Add auto-generated post

---
+++ pages/posts/Dubai Flood Crisis.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Dubai Flood Crisis</h1>
+            <p>In mid-April 2024, Dubai faced unprecedented rainfall, submerging streets and disrupting flights, highlighting climate vulnerabilities. Officials mobilized emergency teams as global discussions on escalating extreme weather events intensify.</p>
+          </article>
+        );
+
+        export default Post;
+        
