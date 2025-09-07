a59050dcc96525ed79fbc650c4f491bed974f9ac
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 21:36:15 +0000

    Add auto-generated post

---
+++ pages/posts/Mideast Ceasefire Agreement.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mideast Ceasefire Agreement</h1>
+            <p>A recent five-day truce between Israel and Hamas facilitated humanitarian aid delivery in Gaza and prisoner exchanges, mediated by Qatar, Egypt, and the US. Despite temporary calm, tensions persist over long-term resolutions and regional stability.</p>
+          </article>
+        );
+
+        export default Post;
+        
