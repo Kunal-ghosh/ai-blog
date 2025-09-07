9119eb22a6c2bc25a69c0da92c051473eab22baf
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 02:47:31 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Landing Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Landing Milestone</h1>
+            <p>Odysseus, a private lunar lander by Intuitive Machines, recently touched down on the Moon, marking the first U.S. spacecraft to land there since 1972. The mission aims to advance Artemis program goals and study lunar surface conditions.</p>
+          </article>
+        );
+
+        export default Post;
+        
