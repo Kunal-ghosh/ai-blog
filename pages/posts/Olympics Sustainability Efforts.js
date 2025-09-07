34f3f8170109665d37b58f1d9a05cb1da5f72801
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 07:14:34 +0000

    Add auto-generated post

---
+++ pages/posts/Olympics Sustainability Efforts.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Olympics Sustainability Efforts</h1>
+            <p>Paris 2024 organizers unveil solar-powered athletes' villages and hydrogen infrastructure, aiming to host the "greenest Games" amid global focus on climate-conscious mega-events.</p>
+          </article>
+        );
+
+        export default Post;
+        
