6c00c03198b8829fea7ef3752fe59846a8c8c205
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 15:15:07 +0000

    Add auto-generated post

---
+++ pages/posts/Space Mission Delay.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Space Mission Delay</h1>
+            <p>NASA and Boeing's Starliner crew return from the ISS delayed due to technical issues, extending astronauts' stay. Teams troubleshoot helium leaks and thruster problems before scheduling re-entry.</p>
+          </article>
+        );
+
+        export default Post;
+        
