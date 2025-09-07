cbe792c09a8ac23982c7f5cf72018895df0041ef
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 10:15:30 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis II Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Update</h1>
+            <p>NASA announces Artemis II crew, targeting a 2024 lunar flyby to prep for future manned Moon landings and Mars missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
