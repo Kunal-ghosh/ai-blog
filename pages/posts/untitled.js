3fa371741c22cc7813db854d0d99d35213686eb5
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-07 20:16:56 +0000

    Add auto-generated post

--- pages/posts/untitled.js
+++ pages/posts/untitled.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>untitled</h1>
-            <p>untitled</p>
+            <p>NASA unveils Artemis II astronauts for 2024 lunar flyby, marking humanity's first Moon mission in 50 years. The crew, including international members, will test systems for future deep-space exploration and Mars ambitions.</p>
           </article>
         );
 
