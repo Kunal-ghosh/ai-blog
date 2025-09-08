ba4ca45ff438cddc3e19e4c8ac9e2447b40a13d6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-08 04:43:14 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Lunar Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Lunar Milestone</h1>
+            <p>NASA's Artemis II mission successfully launched, sending astronauts to lunar orbit for the first time in 50 years. The mission aims to test systems for future crewed Mars expeditions, involving global space agencies.</p>
+          </article>
+        );
+
+        export default Post;
+        
