6dbecaf64982d0467b7c3371d296d7a06eede1b9
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-08 12:37:19 +0000

    Add auto-generated post

---
+++ pages/posts/Hubble's Cosmic Anniversary.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Hubble's Cosmic Anniversary</h1>
+            <p>NASA commemorates Hubble Telescope's 34th year with stunning new images of the Eagle Nebula's "Pillars of Creation," showcasing ongoing astronomical discoveries and cosmic beauty.</p>
+          </article>
+        );
+
+        export default Post;
+        
