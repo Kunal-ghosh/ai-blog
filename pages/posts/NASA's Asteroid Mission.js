19df0082680de16ab4b50952b13ff8c5af775139
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-08 18:47:00 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Asteroid Mission.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Asteroid Mission</h1>
+            <p>NASA's DART spacecraft recently collided with an asteroid to test planetary defense. The successful mission aims to refine strategies for deflecting potential Earth-threatening space objects.</p>
+          </article>
+        );
+
+        export default Post;
+        
