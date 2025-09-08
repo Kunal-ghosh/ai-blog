19dd1c447be2aded244054d14cc549a430a92d45
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-08 23:17:04 +0000

    Add auto-generated post

---
+++ pages/posts/Fukushima Water Release.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Fukushima Water Release</h1>
+            <p>Japan began releasing treated radioactive water from the Fukushima Daiichi nuclear plant into the Pacific Ocean, sparking regional protests. Authorities insist it meets safety standards, but China and South Korea condemn the move amid environmental concerns.</p>
+          </article>
+        );
+
+        export default Post;
+        
