6a9381f7b8d650f43eaa7d2d57227eccd2218f05
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-08 05:40:24 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis II Crew Revealed.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Crew Revealed</h1>
+            <p>NASA announced the final crew for Artemis II, set to orbit the Moon in late 2024. The mission marks humanity’s first lunar voyage in 50 years, featuring an international team of astronauts.</p>
+          </article>
+        );
+
+        export default Post;
+        
