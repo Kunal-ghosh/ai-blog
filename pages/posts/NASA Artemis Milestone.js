c3ec675b43fb4ee335ed542f15a90a4bb7a22dc4
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-08 14:18:26 +0000

    Add auto-generated post

--- pages/posts/NASA Artemis Milestone.js
+++ pages/posts/NASA Artemis Milestone.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>NASA Artemis Milestone</h1>
-            <p>NASA's Artemis II mission advances with crew training, targeting a 2024 lunar flyby to pave the way for future manned Moon landings and deep-space exploration.</p>
+            <p>NASA announces Artemis II crew, targeting a 2024 lunar orbit mission. The first woman and person of color will journey to the Moon.</p>
           </article>
         );
 
