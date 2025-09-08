b56e174fc9a2fdcb2213cde2b4968e337e758969
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-08 08:24:43 +0000

    Add auto-generated post

---
+++ pages/posts/Starliner Setback.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Starliner Setback</h1>
+            <p>NASA astronauts extend ISS stay amid Boeing's Starliner spacecraft delays due to technical challenges, sparking debates on crew safety and mission reliability.</p>
+          </article>
+        );
+
+        export default Post;
+        
