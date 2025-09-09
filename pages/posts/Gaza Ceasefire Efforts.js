014e2f06478d58b4ff32cc61aa6477be1593c5c5
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 23:15:05 +0000

    Add auto-generated post

---
+++ pages/posts/Gaza Ceasefire Efforts.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Gaza Ceasefire Efforts</h1>
+            <p>Egypt brokers new Israel-Hamas ceasefire talks amid escalating tensions, aiming to halt recent violence. International leaders urge humanitarian aid access and renewed diplomatic solutions.</p>
+          </article>
+        );
+
+        export default Post;
+        
