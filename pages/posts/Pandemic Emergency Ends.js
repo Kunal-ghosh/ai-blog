9c6c0044f78970bceb7b2df349f6465fac505792
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 21:36:23 +0000

    Add auto-generated post

---
+++ pages/posts/Pandemic Emergency Ends.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Pandemic Emergency Ends</h1>
+            <p>The World Health Organization (WHO) declared the end of COVID-19 as a global health emergency on May 5, 2023, marking a major milestone. Nations now shift focus to long-term management amid ongoing health and economic impacts.</p>
+          </article>
+        );
+
+        export default Post;
+        
