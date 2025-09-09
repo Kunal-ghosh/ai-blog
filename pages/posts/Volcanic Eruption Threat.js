508e05fcd3c4741f21da8a66e1d664f801461791
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 19:13:39 +0000

    Add auto-generated post

---
+++ pages/posts/Volcanic Eruption Threat.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Volcanic Eruption Threat</h1>
+            <p>Iceland's Reykjanes Peninsula experienced a volcanic eruption on May 29, 2024, spewing lava and ash near Grindavík. Evacuations and flight disruptions ensued as scientists monitor potential long-term geothermal activity risks.</p>
+          </article>
+        );
+
+        export default Post;
+        
