28386684e8ade858021b4a419528b57f68c06066
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 04:21:26 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Mission Failure.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Mission Failure</h1>
+            <p>Russia's Luna-25 spacecraft crashed into the Moon after spinning into an uncontrolled orbit, marking its first lunar mission failure in 47 years. The incident highlights challenges in reviving Russia’s once-dominant space exploration legacy.</p>
+          </article>
+        );
+
+        export default Post;
+        
