723904a7e82c39fac4c7db42efefd1a99ac77401
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 07:16:54 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Artemis II Announcement.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Artemis II Announcement</h1>
+            <p>NASA unveiled the Artemis II crew, targeting a 2024 lunar mission. The diverse team, including the first woman and person of color, will orbit the Moon, paving the way for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
