b4e268fe2f8a7d7bef4fc2a4b3218c2644b0b9aa
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 20:38:26 +0000

    Add auto-generated post

---
+++ pages/posts/US Debt Deal.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>US Debt Deal</h1>
+            <p>Congress passes bipartisan debt ceiling agreement, averting potential default crisis. Critics cite spending concerns, while supporters emphasize economic stability secured.</p>
+          </article>
+        );
+
+        export default Post;
+        
