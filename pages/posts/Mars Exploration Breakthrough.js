b45d58c0ace8bba5915b76401d6afc8b2310d977
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 06:26:56 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Exploration Breakthrough.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Exploration Breakthrough</h1>
+            <p>NASA's Perseverance rover recently discovered organic molecules in Martian rock samples, hinting at potential ancient microbial life. This finding fuels global excitement about uncovering Mars' habitability secrets.</p>
+          </article>
+        );
+
+        export default Post;
+        
