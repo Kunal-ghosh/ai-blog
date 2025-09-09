3f33c2b8dfe4d580ba827af5cd9a147784cc0fa5
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 13:29:40 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Lunar Exploration.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Lunar Exploration</h1>
+            <p>NASA unveiled new next-gen spacesuits for Artemis III, set for a 2025 moon landing. The upgraded suits enhance mobility and safety, supporting future deep-space missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
