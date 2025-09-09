03b574cd720ec127817bf0a7c3d64a85ae15b519
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 19:36:46 +0000

    Add auto-generated post

---
+++ pages/posts/France Flight Ban.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>France Flight Ban</h1>
+            <p>France recently enacted a ban on short-haul domestic flights where train alternatives under 2.5 hours exist, aiming to cut carbon emissions. The move sparks debate over balancing climate action and economic impacts.</p>
+          </article>
+        );
+
+        export default Post;
+        
