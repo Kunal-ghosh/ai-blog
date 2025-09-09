a9168c965b08a458e611eb65fed39ddb59a8ddd7
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 15:41:07 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Mission Collaboration.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Mission Collaboration</h1>
+            <p>NASA and ESA announce joint Mars sample-return mission, aiming to bring Martian soil to Earth by 2033. The unprecedented effort seeks to uncover clues about ancient microbial life and planetary evolution.</p>
+          </article>
+        );
+
+        export default Post;
+        
