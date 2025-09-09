562c40def09475f65de8289d4a9ab3545195a269
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-09 14:42:25 +0000

    Add auto-generated post

---
+++ pages/posts/Historic Mexican Election.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Historic Mexican Election</h1>
+            <p>Claudia Sheinbaum elected as Mexico's first female president, marking a historic shift in leadership. Her victory emphasizes climate action and gender equality as top priorities.</p>
+          </article>
+        );
+
+        export default Post;
+        
