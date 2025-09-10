c6c875c3de461abc96c80ac1365528a17c0fe8d2
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-10 14:40:42 +0000

    Add auto-generated post

--- pages/posts/Artemis Moon Mission.js
+++ pages/posts/Artemis Moon Mission.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis Moon Mission</h1>
-            <p>NASA announces Artemis II crewed mission, set for 2025, aiming to orbit the Moon. The mission paves the way for future lunar exploration and Mars ambitions.</p>
+            <p>NASA announced Artemis II crew for a 2024 lunar flyby, marking humanity's first moon mission in 50 years. Astronauts will test systems for future deep-space exploration and sustained lunar presence.</p>
           </article>
         );
 
