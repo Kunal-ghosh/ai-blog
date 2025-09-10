e2839cf6f0ba04eb4fc0719e8c54da496b23d334
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-10 06:25:52 +0000

    Add auto-generated post

---
+++ pages/posts/Tesla Cybertruck Recall.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Tesla Cybertruck Recall</h1>
+            <p>Tesla recalls over 11,000 Cybertrucks due to faulty accelerator pedal issue, prompting safety concerns. The NHTSA investigation highlights growing scrutiny over electric vehicle manufacturing standards.</p>
+          </article>
+        );
+
+        export default Post;
+        
