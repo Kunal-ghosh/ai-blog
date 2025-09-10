e65e2d8e66dc380b43f730bf35962324a9ca0d0e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-10 23:36:38 +0000

    Add auto-generated post

---
+++ pages/posts/Caribbean Hurricane Destruction.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Caribbean Hurricane Destruction</h1>
+            <p>Hurricane Beryl, the earliest Category 5 storm recorded, devastated Caribbean islands, causing widespread damage and displacement. Emergency crews scramble to deliver aid amid rising climate crisis concerns.</p>
+          </article>
+        );
+
+        export default Post;
+        
