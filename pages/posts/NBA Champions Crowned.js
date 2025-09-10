8d9e96556ea4a5e8dbd653940c4eb5abc06069fe
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-10 03:26:05 +0000

    Add auto-generated post

---
+++ pages/posts/NBA Champions Crowned.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NBA Champions Crowned</h1>
+            <p>The Boston Celtics secured their record 18th NBA championship, defeating the Dallas Mavericks in a dominant Finals series. Luka Dončić’s historic 32-point triple-double wasn’t enough to stop Boston’s clinching Game 5 victory.</p>
+          </article>
+        );
+
+        export default Post;
+        
