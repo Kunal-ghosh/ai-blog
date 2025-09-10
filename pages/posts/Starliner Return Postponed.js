03c5331c1a23283afb7b841d7a165cf0ee45e214
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-10 02:40:18 +0000

    Add auto-generated post

---
+++ pages/posts/Starliner Return Postponed.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Starliner Return Postponed</h1>
+            <p>NASA delayed Boeing’s Starliner astronaut mission return to review thruster and helium leak issues. Crew remains safe aboard the ISS as teams collaborate with SpaceX for contingency plans, highlighting challenges in spaceflight.</p>
+          </article>
+        );
+
+        export default Post;
+        
