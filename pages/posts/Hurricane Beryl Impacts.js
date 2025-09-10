9fb981370ed0e2889cdc4a4a8ea3a91fc19f63e9
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-10 05:16:31 +0000

    Add auto-generated post

---
+++ pages/posts/Hurricane Beryl Impacts.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Hurricane Beryl Impacts</h1>
+            <p>Hurricane Beryl intensified to a Category 5 storm, tearing through the Caribbean with destructive winds and floods. Authorities warn of continued risks as it approaches Mexico, urging mass evacuations in vulnerable regions.</p>
+          </article>
+        );
+
+        export default Post;
+        
