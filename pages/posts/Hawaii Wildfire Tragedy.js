7c9d22c1e6db463ea396e5f955ba684e2b0a8b73
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-10 19:13:36 +0000

    Add auto-generated post

---
+++ pages/posts/Hawaii Wildfire Tragedy.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Hawaii Wildfire Tragedy</h1>
+            <p>Deadly wildfires ravage Maui, Hawaii, killing over 100 and displacing thousands amid drought and high winds. Emergency crews battle blazes as global leaders call for climate action.</p>
+          </article>
+        );
+
+        export default Post;
+        
