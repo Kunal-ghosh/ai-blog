57ca15ef6cbadcf8d4d00e3f1c2b267299a7763e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-10 15:18:03 +0000

    Add auto-generated post

---
+++ pages/posts/UK Election Shift.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>UK Election Shift</h1>
+            <p>Labour Party secures landslide victory in UK general election, ending 14 years of Conservative rule. New PM Keir Starmer pledges focus on economic reform and climate action amid global watch.</p>
+          </article>
+        );
+
+        export default Post;
+        
