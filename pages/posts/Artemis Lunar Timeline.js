2f55a4c20e7b1de191a2168acafa36e28bb3339e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 22:15:42 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Lunar Timeline.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Lunar Timeline</h1>
+            <p>NASA announced revised plans for its Artemis program, targeting a 2026 crewed Moon landing. Delays address safety concerns and technical challenges, prioritizing sustainable lunar exploration and international collaboration.</p>
+          </article>
+        );
+
+        export default Post;
+        
