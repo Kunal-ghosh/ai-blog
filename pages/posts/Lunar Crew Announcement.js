a0423a4a8bca83cf71963714c98aaf1ffc7126e8
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 03:31:20 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Crew Announcement.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Crew Announcement</h1>
+            <p>NASA unveiled the Artemis II crew, the first lunar mission in 50 years, set to orbit the Moon in 2024. The diverse team aims to pave the way for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
