b5bbebc2b297afe8f871527438ebf82da10b4d35
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 09:18:19 +0000

    Add auto-generated post

---
+++ pages/posts/Submersible Implosion Tragedy.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Submersible Implosion Tragedy</h1>
+            <p>A tourist submersible exploring the Titanic wreckage imploded near Newfoundland, killing all five aboard. International teams recovered debris, highlighting deep-sea exploration risks.</p>
+          </article>
+        );
+
+        export default Post;
+        
