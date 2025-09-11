b299569f6ff6f4f3b502b99ee23de6c8aba4b82f
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 23:36:27 +0000

    Add auto-generated post

---
+++ pages/posts/Global Heatwaves Worsen.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Global Heatwaves Worsen</h1>
+            <p>April 2024 shattered temperature records worldwide, intensifying droughts and wildfires from Europe to Asia. Experts warn accelerating climate change demands urgent global policy shifts to mitigate irreversible ecological damage.</p>
+          </article>
+        );
+
+        export default Post;
+        
