b26c4d3ccc0a1c45e7447dd589dea7f337fcba4f
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 17:36:09 +0000

    Add auto-generated post

--- pages/posts/Artemis II Crew Announcement.js
+++ pages/posts/Artemis II Crew Announcement.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Crew Announcement</h1>
-            <p>NASA reveals international crew for Artemis II, targeting a 2024 lunar flyby. The mission marks humanity's first return to the Moon in over 50 years.</p>
+            <p>NASA revealed the international crew for Artemis II, targeting a 2024 lunar flyby. The mission paves the way for future moon landings and Mars exploration.</p>
           </article>
         );
 
