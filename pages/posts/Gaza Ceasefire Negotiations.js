6f5f161a898e46b6e82c3ee86f16fa2266af45a3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 08:44:38 +0000

    Add auto-generated post

---
+++ pages/posts/Gaza Ceasefire Negotiations.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Gaza Ceasefire Negotiations</h1>
+            <p>Egypt and Qatar mediate renewed Israel-Hamas ceasefire talks as humanitarian aid efforts intensify amid rising civilian casualties.</p>
+          </article>
+        );
+
+        export default Post;
+        
