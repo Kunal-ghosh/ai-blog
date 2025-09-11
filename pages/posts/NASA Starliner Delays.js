fc1bf1ef3d373baaac7f3887f31ce69a00d9b3cc
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 21:14:44 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Starliner Delays.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Starliner Delays</h1>
+            <p>NASA astronauts Sunita Williams and Butch Wilmore remain aboard the ISS as Boeing's Starliner faces prolonged delays due to technical issues. Ongoing assessments aim to resolve helium leaks and thruster faults before their return.</p>
+          </article>
+        );
+
+        export default Post;
+        
