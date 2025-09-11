5202c884c92ac954b770ffa2864f8a9a92844c4d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 12:34:15 +0000

    Add auto-generated post

--- pages/posts/Moon Mission Update.js
+++ pages/posts/Moon Mission Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Moon Mission Update</h1>
-            <p>NASA announces Artemis II crewed lunar flyby set for 2025, marking humanity's return to the Moon. The mission aims to test systems for future deep-space exploration, involving international collaboration.</p>
+            <p>NASA unveils Artemis II crew, targeting a 2024 lunar flyby. The mission marks the first crewed moon voyage in 50 years, aiming to pave the way for future lunar exploration.</p>
           </article>
         );
 
