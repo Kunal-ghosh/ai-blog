ca37e399c6adf7d314542b0a19c12c79e76404c6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 17:14:20 +0000

    Add auto-generated post

--- pages/posts/Artemis II Crew.js
+++ pages/posts/Artemis II Crew.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Crew</h1>
-            <p>NASA unveiled the Artemis II crew, targeting a 2024 lunar flyby with the first woman and person of color. The mission marks a pivotal step toward sustainable Moon exploration and deep-space ambitions.</p>
+            <p>NASA unveils diverse Artemis II crew, targeting a 2024 lunar flyby. The mission marks humanity's first moon voyage in 50 years, paving the way for future Mars exploration.</p>
           </article>
         );
 
