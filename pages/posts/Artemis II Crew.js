8e3d735ce2285f2b415ce9d66af198787e5fb5de
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 08:22:50 +0000

    Add auto-generated post

--- pages/posts/Artemis II Crew.js
+++ pages/posts/Artemis II Crew.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Crew</h1>
-            <p>NASA recently unveiled the crew for Artemis II, its first manned lunar mission in 50 years. The四人-member team will orbit the Moon in late 2024, paving the way for future Mars exploration.</p>
+            <p>NASA introduces four astronauts for 2024 lunar mission, marking the first crewed Moon flight in 50 years. The Artemis II mission will orbit the Moon, paving the way for future deep-space exploration.</p>
           </article>
         );
 
