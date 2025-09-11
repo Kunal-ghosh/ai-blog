e97c419f390036e80b77ec6558c1817e4c6bd570
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-11 23:15:34 +0000

    Add auto-generated post

---
+++ pages/posts/Spain Triumphs Euro 2024.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Spain Triumphs Euro 2024</h1>
+            <p>Spain clinched the Euro 2024 title with a 2-1 victory over England, securing their fourth European Championship. England's wait for a major football trophy extends despite a spirited final performance.</p>
+          </article>
+        );
+
+        export default Post;
+        
