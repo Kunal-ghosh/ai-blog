2285cc58fa6304125ebd0bb464ef3e5e714ff113
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-12 22:15:03 +0000

    Add auto-generated post

---
+++ pages/posts/Tokyo Airport Collision.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Tokyo Airport Collision</h1>
+            <p>A Japan Airlines passenger plane collided with a Coast Guard aircraft at Tokyo Haneda Airport, erupting in flames. All 379 passengers evacuated safely, while five Coast Guard crew members died; Japan launched an investigation.</p>
+          </article>
+        );
+
+        export default Post;
+        
