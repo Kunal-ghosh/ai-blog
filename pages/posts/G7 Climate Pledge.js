4e3610ae5cc9b4026bdb13ffb3afb9d89718bf83
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-12 05:38:41 +0000

    Add auto-generated post

---
+++ pages/posts/G7 Climate Pledge.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>G7 Climate Pledge</h1>
+            <p>G7 nations recently committed to phase out coal-fired power plants by 2035 and pledged $2 billion for renewable energy projects in developing countries, aiming to accelerate global climate action.</p>
+          </article>
+        );
+
+        export default Post;
+        
