dbdb7ac00942aa0ca5f58fda261f673c6362ea63
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-12 23:14:52 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis II Progress.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis II Progress</h1>
+            <p>NASA and international partners advance preparations for the Artemis II mission, aiming to launch astronauts on a lunar flyby in late 2024, marking humanity’s return to deep space exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
