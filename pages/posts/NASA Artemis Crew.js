567536182e879f4a0777c5d2cee7722e28352b9b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-12 06:47:20 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Artemis Crew.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Artemis Crew</h1>
+            <p>NASA unveiled the four-member Artemis II crew, including the first woman and person of color, for a 2024 lunar flyby mission, advancing humanity's return to the Moon.</p>
+          </article>
+        );
+
+        export default Post;
+        
