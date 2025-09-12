49f3caf933dd29d3c73fabf3eb73fcf78f3c7be8
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-12 01:14:55 +0000

    Add auto-generated post

---
+++ pages/posts/Copa America Chaos.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Copa America Chaos</h1>
+            <p>Crowd disruptions halted the 2024 Copa America final in Miami, delaying the match for 30 minutes. Argentina clinched the title amid heightened security concerns after fans breached gates, injuring several players.</p>
+          </article>
+        );
+
+        export default Post;
+        
