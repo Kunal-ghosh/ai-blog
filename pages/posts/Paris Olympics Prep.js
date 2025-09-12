fc1f2c542258a4e7ee9826cb8dc41e0f3286b63c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-12 20:38:07 +0000

    Add auto-generated post

---
+++ pages/posts/Paris Olympics Prep.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Paris Olympics Prep</h1>
+            <p>With the 2024 Paris Olympics approaching, organizers unveil AI-driven security systems and eco-friendly venues amidst global anticipation. Critics highlight challenges over cost overruns and logistical strains.</p>
+          </article>
+        );
+
+        export default Post;
+        
