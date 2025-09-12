f5aac6c633ecedd8c0cfe3f3ad3d39de11e543b1
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-12 10:40:32 +0000

    Add auto-generated post

---
+++ pages/posts/France Election Turmoil.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>France Election Turmoil</h1>
+            <p>France's snap election called by President Macron following EU Parliament shifts sparks political uncertainty. The upcoming vote, driven by far-right gains, has triggered protests and market fluctuations globally.</p>
+          </article>
+        );
+
+        export default Post;
+        
