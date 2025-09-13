ec6feabdc5d00fd2970bc311f4e481591c252046
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-13 22:14:27 +0000

    Add auto-generated post

---
+++ pages/posts/Middle East Conflict.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Middle East Conflict</h1>
+            <p>Recent escalations in the Israel-Gaza war have prompted renewed ceasefire talks, mediated by Egypt and Qatar, amid rising global calls to address civilian casualties and humanitarian aid shortages. Clashes and airstrikes continue, with over 30,000 reported displaced in the past week.</p>
+          </article>
+        );
+
+        export default Post;
+        
