da2f2b9f21a777efb7c771074d16e873ebe6bf2a
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-13 08:40:46 +0000

    Add auto-generated post

---
+++ pages/posts/EU Elections Outcome.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>EU Elections Outcome</h1>
+            <p>The 2024 European Parliament elections concluded with centrist and Green parties securing a majority, signaling strong voter support for climate action and cross-border unity. Far-right gains in several member states, however, underscored ongoing tensions over immigration and sovereignty.</p>
+          </article>
+        );
+
+        export default Post;
+        
