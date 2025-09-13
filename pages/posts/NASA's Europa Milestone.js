b6679a185e57dac051f3122d9147d9d96d85fb0d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-13 20:36:20 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Europa Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Europa Milestone</h1>
+            <p>NASA recently installed a high-gain antenna on its Europa Clipper spacecraft, set to launch in 2024 to explore Jupiter’s icy moon. The mission aims to study Europa’s subsurface ocean for potential signs of habitable conditions.</p>
+          </article>
+        );
+
+        export default Post;
+        
