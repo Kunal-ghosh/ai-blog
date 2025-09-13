21df05540833715bfc226eeeaa3481086e5230bc
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-13 22:36:49 +0000

    Add auto-generated post

--- pages/posts/NASA Artemis Update.js
+++ pages/posts/NASA Artemis Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>NASA Artemis Update</h1>
-            <p>NASA unveils Artemis II crew, targeting a 2024 lunar flyby. The mission marks humanity's first moon voyage since 1972, featuring a diverse international team.</p>
+            <p>NASA unveils Artemis II crew, prepping for 2024 moon mission.</p>
           </article>
         );
 
