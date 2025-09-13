3f12522d69b71210b93815c61cb18daad58e06f6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-13 07:36:39 +0000

    Add auto-generated post

---
+++ pages/posts/Global Climate Initiative.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Global Climate Initiative</h1>
+            <p>NASA and UAE unveiled a joint space-based climate monitoring system, targeting global emission reductions. The project aims to enhance data accuracy for Paris Agreement goals, using advanced satellite tech.</p>
+          </article>
+        );
+
+        export default Post;
+        
