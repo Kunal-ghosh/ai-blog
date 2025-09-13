fe431f8357e0dabe09f070ea535e63324c64b6cf
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-13 08:19:43 +0000

    Add auto-generated post

--- pages/posts/Lunar Exploration Milestone.js
+++ pages/posts/Lunar Exploration Milestone.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Lunar Exploration Milestone</h1>
-            <p>NASA's Artemis II mission advances, finalizing crew training for a 2024 lunar flyby. Collaboration with international agencies aims to establish sustained Moon presence by decade's end.</p>
+            <p>NASA announces Artemis II crew training progress, targeting a 2024 lunar orbit mission. The effort aims to pave the way for future human Mars exploration.</p>
           </article>
         );
 
