4ef29785ae51166a51dca6b5d12eced75674384b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-14 14:36:39 +0000

    Add auto-generated post

---
+++ pages/posts/Twitter Rebranded as X.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Twitter Rebranded as X</h1>
+            <p>Elon Musk's rebranding of Twitter to "X" sparked global debate, replacing the iconic bird logo with a minimalist X. Critics question the move's strategic value, while supporters see it as a step toward Musk's "everything app" vision.</p>
+          </article>
+        );
+
+        export default Post;
+        
