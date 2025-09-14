579463cf62be601a255451040a2681062a522549
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-14 09:15:31 +0000

    Add auto-generated post

---
+++ pages/posts/Climate Crisis Escalation.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Crisis Escalation</h1>
+            <p>Temperatures soar globally as July 2023 confirmed the hottest month on record, intensifying wildfires and floods. Scientists urge immediate climate action to curb irreversible environmental damage.</p>
+          </article>
+        );
+
+        export default Post;
+        
