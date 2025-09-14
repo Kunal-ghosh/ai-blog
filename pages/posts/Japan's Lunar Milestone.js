5e575595467788870c3046854ac8d6d1258e9d8d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-14 10:15:14 +0000

    Add auto-generated post

---
+++ pages/posts/Japan's Lunar Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Japan's Lunar Milestone</h1>
+            <p>Japan made history as its SLIM spacecraft achieved a precise lunar landing, becoming the fifth nation to reach the Moon. The mission aims to test cutting-edge navigation tech for future deep-space exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
