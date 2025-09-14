ae4b705830cdc8d4d5ca5d81fd4dad34229b74e8
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-14 11:36:21 +0000

    Add auto-generated post

---
+++ pages/posts/Olympic Mascot Debut.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Olympic Mascot Debut</h1>
+            <p>Paris 2024 introduces "Les Phryges," quirky red mascots symbolizing freedom and revolution, blending French flair with Olympic spirit. The phrygian cap-inspired characters aim to champion inclusivity and sustainability ahead of the Games.</p>
+          </article>
+        );
+
+        export default Post;
+        
