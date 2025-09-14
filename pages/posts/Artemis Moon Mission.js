68d6dbe38b4f731a09b4dd27b09965e5f3957457
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-14 10:36:52 +0000

    Add auto-generated post

--- pages/posts/Artemis Moon Mission.js
+++ pages/posts/Artemis Moon Mission.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis Moon Mission</h1>
-            <p>NASA's Artemis II crew prepares for a 2024 lunar orbit, aiming to pave the way for future manned Mars missions. The mission marks humanity's first return to the Moon in over 50 years.</p>
+            <p>NASA announced Artemis II crew's 2024 lunar orbit mission, advancing human deep space exploration. The milestone paves the way for future Mars expeditions.</p>
           </article>
         );
 
