f5f54088bc7ead22bf280e19fc7fccf0dbd91cc0
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-14 18:21:02 +0000

    Add auto-generated post

---
+++ pages/posts/Moon Mission Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Moon Mission Milestone</h1>
+            <p>NASA's Artemis II successfully completed its lunar flyby test, paving the way for crewed missions in 2025. Astronauts will orbit the moon, marking humanity’s return since 1972.</p>
+          </article>
+        );
+
+        export default Post;
+        
