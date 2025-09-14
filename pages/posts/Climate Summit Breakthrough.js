c43798503a426e96220d9cdfd714e646457d2b63
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-14 15:14:41 +0000

    Add auto-generated post

---
+++ pages/posts/Climate Summit Breakthrough.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Summit Breakthrough</h1>
+            <p>Global leaders at COP28 finalized a historic agreement to triple renewable energy by 2030, aiming to curb emissions. The deal faces criticism, however, over exemptions for fossil fuel-dependent nations.</p>
+          </article>
+        );
+
+        export default Post;
+        
