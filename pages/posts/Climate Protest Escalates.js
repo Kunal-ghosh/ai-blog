711edb3e86612219c23494eeb45dcbd5ff292802
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-14 23:14:28 +0000

    Add auto-generated post

---
+++ pages/posts/Climate Protest Escalates.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Protest Escalates</h1>
+            <p>Just Stop Oil activists sprayed Stonehenge with orange paint, demanding urgent climate action. The incident sparked global debate over protest methods and environmental priorities.</p>
+          </article>
+        );
+
+        export default Post;
+        
