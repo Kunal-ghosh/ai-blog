b15c36e055135fa63a41c7292a4047f7a1bea862
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 22:15:03 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Lunar Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Lunar Update</h1>
+            <p>NASA announced the Artemis II mission's four-person crew, targeting a 2025 lunar flyby to pave the way for human moon landings. The mission aims to establish sustainable lunar exploration and test technologies for future Mars expeditions.</p>
+          </article>
+        );
+
+        export default Post;
+        
