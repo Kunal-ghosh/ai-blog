bdd998f4e85e6115deda230cb66b5b4249576294
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 16:45:13 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Mission Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Mission Milestone</h1>
+            <p>NASA's Ingenuity helicopter concludes its 3-year Mars mission, paving the way for future aerial exploration. The rotorcraft exceeded expectations, completing 72 flights and proving powered flight on another planet.</p>
+          </article>
+        );
+
+        export default Post;
+        
