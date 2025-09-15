a0d9e8948aba9741230e3418bffb436437eb5eb1
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 10:18:39 +0000

    Add auto-generated post

---
+++ pages/posts/New Cosmic Discoveries.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>New Cosmic Discoveries</h1>
+            <p>The James Webb Space Telescope recently captured unprecedented high-resolution images of the Orion Nebula, revealing star formation details. Scientists hail the data as transformative for understanding stellar evolution and planetary systems.</p>
+          </article>
+        );
+
+        export default Post;
+        
