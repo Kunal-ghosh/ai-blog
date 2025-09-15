8e330a406e0b8645760e5a63ec2a062080df3897
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 19:13:22 +0000

    Add auto-generated post

---
+++ pages/posts/Middle East Peace Talks.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Middle East Peace Talks</h1>
+            <p>Egypt and Qatar broker renewed negotiations between Israel and Hamas, aiming for a ceasefire and hostage exchange after months of stalemate. International leaders urge compromise as humanitarian crises escalate in Gaza.</p>
+          </article>
+        );
+
+        export default Post;
+        
