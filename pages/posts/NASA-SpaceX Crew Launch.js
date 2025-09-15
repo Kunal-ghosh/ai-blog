79decc7ddf201dc62017e8f67377efaeecc99b4b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 01:22:06 +0000

    Add auto-generated post

---
+++ pages/posts/NASA-SpaceX Crew Launch.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA-SpaceX Crew Launch</h1>
+            <p>NASA and SpaceX successfully launched the Crew-7 mission, sending four astronauts to the ISS. The diverse team will conduct scientific experiments to advance long-term space exploration goals.</p>
+          </article>
+        );
+
+        export default Post;
+        
