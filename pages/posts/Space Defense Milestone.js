8e9afc836457ba8f7e96bc1126542e2a3787f1cb
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 07:38:59 +0000

    Add auto-generated post

---
+++ pages/posts/Space Defense Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Space Defense Milestone</h1>
+            <p>NASA's DART mission successfully altered an asteroid's orbit, marking humanity's first test of planetary defense by deflecting space rocks. The achievement advances Earth's ability to counter potential cosmic threats.</p>
+          </article>
+        );
+
+        export default Post;
+        
