852d1407a7c7af9214d72302407b0935d6f9f6e0
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 04:21:19 +0000

    Add auto-generated post

--- pages/posts/Artemis II Milestone.js
+++ pages/posts/Artemis II Milestone.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Milestone</h1>
-            <p>NASA announces Artemis II crew, targeting a 2024 lunar flyby. The mission aims to pave the way for sustainable Moon exploration and future Mars missions.</p>
+            <p>NASA's Artemis II crew completes intensive training, sets stage for November 2024 lunar flyby. First human moon mission since 1972 sparks global interest in renewed space exploration.</p>
           </article>
         );
 
