e4c2ebb2a1a49d79a0ca5cd7e79cb87eae7fb27d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 05:40:41 +0000

    Add auto-generated post

---
+++ pages/posts/Global Climate Commitment.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Global Climate Commitment</h1>
+            <p>World leaders at COP28 reached a historic agreement to phase out fossil fuels by 2050, while pledging $700 billion to accelerate renewable energy adoption globally.</p>
+          </article>
+        );
+
+        export default Post;
+        
