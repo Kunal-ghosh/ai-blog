79921fdcb0549057ec0f5c7a72b489cb8ec8c833
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-15 04:42:57 +0000

    Add auto-generated post

---
+++ pages/posts/Asteroid Defense Drill.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Asteroid Defense Drill</h1>
+            <p>NASA recently conducted a global asteroid impact preparedness exercise, simulating detection and response to a hypothetical near-Earth object threat. Over 100 international experts collaborated to assess defense strategies and improve planetary protection measures.</p>
+          </article>
+        );
+
+        export default Post;
+        
