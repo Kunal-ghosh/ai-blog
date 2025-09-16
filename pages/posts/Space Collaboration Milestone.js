6a221f3c1a2bcfb3cbd87794e530a0593300472e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 21:15:23 +0000

    Add auto-generated post

---
+++ pages/posts/Space Collaboration Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Space Collaboration Milestone</h1>
+            <p>NASA and SpaceX successfully launched Crew-7, ushering in a new era of international space exploration. The mission, carrying astronauts from four nations, aims to conduct advanced research aboard the ISS.</p>
+          </article>
+        );
+
+        export default Post;
+        
