564dd91faede2e9c010123c21017f016a7806166
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 20:39:40 +0000

    Add auto-generated post

---
+++ pages/posts/Euro 2024 Champions.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Euro 2024 Champions</h1>
+            <p>Spain clinched victory in the Euro 2024 final, defeating England 2-1 in a thrilling match. This marks Spain's first major tournament win in 12 years, reigniting football fervor globally.</p>
+          </article>
+        );
+
+        export default Post;
+        
