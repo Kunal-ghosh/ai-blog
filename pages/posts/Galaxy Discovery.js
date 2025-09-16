3421f093ceac3d6a760683a9557dabd150cc6b8a
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 22:15:20 +0000

    Add auto-generated post

---
+++ pages/posts/Galaxy Discovery.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Galaxy Discovery</h1>
+            <p>James Webb Telescope captures unprecedented images of a distant galaxy, revealing new star formations. Scientists say the data could reshape understanding of early universe evolution.</p>
+          </article>
+        );
+
+        export default Post;
+        
