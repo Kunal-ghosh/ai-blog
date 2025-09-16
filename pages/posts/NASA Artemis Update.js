2eeb55e5a54b1f47a93c49418c6835a8c6dccac1
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 09:19:16 +0000

    Add auto-generated post

--- pages/posts/NASA Artemis Update.js
+++ pages/posts/NASA Artemis Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>NASA Artemis Update</h1>
-            <p>NASA unveils Artemis II crew, prepping for 2024 moon mission.</p>
+            <p>NASA unveils plans for Artemis III, aiming to land astronauts near the Moon's south pole by 2025. The mission seeks to establish sustainable lunar exploration and pave the way for future Mars expeditions.</p>
           </article>
         );
 
