e6e9e91288c30605b59c7c5db7a8ff6693d8b1b9
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 08:23:57 +0000

    Add auto-generated post

---
+++ pages/posts/Climate Activism Surge.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Activism Surge</h1>
+            <p>Thousands of global activists rallied at UN headquarters, demanding immediate action on fossil fuel phaseouts ahead of COP29. Protests highlighted escalating pressure on governments to prioritize climate justice and enforceable emission targets.</p>
+          </article>
+        );
+
+        export default Post;
+        
