4e0b1e855c5ff5708799efaf45e9756be7358483
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 02:41:05 +0000

    Add auto-generated post

---
+++ pages/posts/Middle East Ceasefire Deal.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Middle East Ceasefire Deal</h1>
+            <p>Egypt-brokered truce between Israel and Hamas halts weeks of violence, aiming to ease humanitarian crises. Global leaders urge sustained dialogue amid fragile hopes for lasting peace.</p>
+          </article>
+        );
+
+        export default Post;
+        
