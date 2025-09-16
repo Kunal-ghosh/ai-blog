f2894436091c9f56ff4607109d5a1da03d04d3d2
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 14:42:49 +0000

    Add auto-generated post

--- pages/posts/Artemis Mission Update.js
+++ pages/posts/Artemis Mission Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis Mission Update</h1>
-            <p>NASA's Artemis II completed a successful lunar flyby test, advancing plans for a crewed Moon landing. The mission aims to return astronauts to the lunar surface by late 2024.</p>
+            <p>NASA's Artemis II mission prepares for a 2025 lunar flyby, paving the way for future crewed Moon landings. International collaboration and advanced spacecraft aim to establish sustained lunar exploration.</p>
           </article>
         );
 
