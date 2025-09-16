d8ad0d6de6e640d063359e5a058c2c744668629b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 04:41:08 +0000

    Add auto-generated post

---
+++ pages/posts/NATO Summit Updates.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NATO Summit Updates</h1>
+            <p>World leaders at the 2024 NATO Summit emphasized bolstering collective defense amid rising tensions, pledging increased military spending and aid to Ukraine. New strategies to counter Russia’s aggression and Sweden’s formal induction marked key highlights.</p>
+          </article>
+        );
+
+        export default Post;
+        
