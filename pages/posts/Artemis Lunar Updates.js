2c2f2618a8e7e45595f5937155b180bfa4dfb7ce
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 18:24:51 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis Lunar Updates.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis Lunar Updates</h1>
+            <p>NASA announces Artemis II crewed mission, targeting a 2024 lunar orbit. The mission paves the way for future moon landings and deep-space exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
