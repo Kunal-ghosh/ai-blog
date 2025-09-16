0e57bfee93fa81fd08506c3a3c950e557451fc22
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 08:46:32 +0000

    Add auto-generated post

---
+++ pages/posts/UAE Flood Crisis.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>UAE Flood Crisis</h1>
+            <p>Record-breaking rainfall triggers flash floods in UAE, submerging major highways and disrupting Dubai Airport. Authorities deploy emergency response teams as climate experts cite unprecedented weather patterns.</p>
+          </article>
+        );
+
+        export default Post;
+        
