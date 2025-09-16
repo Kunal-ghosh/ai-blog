8ac1a349bdec70ce8f4a102af075cb4c56b6edbe
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 15:18:53 +0000

    Add auto-generated post

---
+++ pages/posts/Israel Escalates Gaza Offensive.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Israel Escalates Gaza Offensive</h1>
+            <p>Israeli forces intensified ground operations in Gaza, targeting Hamas infrastructure amid rising civilian casualties. International calls for ceasefire grow as aid groups warn of worsening humanitarian crisis.</p>
+          </article>
+        );
+
+        export default Post;
+        
