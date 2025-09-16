f63fa0201083d81bcab49726aed92c2ba68e7539
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 06:49:35 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Mars Mission.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Mars Mission</h1>
+            <p>NASA's Ingenuity helicopter concludes its historic mission after 72 flights on Mars, exceeding initial expectations. The groundbreaking rotorcraft paved the way for future aerial exploration of the Red Planet.</p>
+          </article>
+        );
+
+        export default Post;
+        
