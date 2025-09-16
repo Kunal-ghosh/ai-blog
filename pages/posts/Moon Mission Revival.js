d238f832c7dbb108666d0bb0b8ef85ffe45272a1
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-16 07:37:24 +0000

    Add auto-generated post

---
+++ pages/posts/Moon Mission Revival.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Moon Mission Revival</h1>
+            <p>NASA unveils Artemis II crew, targeting a 2024 lunar flyby—the first human moon mission in 50 years. The Orion spacecraft will test systems for future deep-space exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
