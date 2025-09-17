a87b8e41ca503842b9139ad69edfb48c41d2e145
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-17 14:16:45 +0000

    Add auto-generated post

---
+++ pages/posts/Galaxy Discovery News.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Galaxy Discovery News</h1>
+            <p>James Webb Space Telescope uncovers ancient galaxies, reshaping cosmic understanding. The breakthrough reveals stars forming just 290 million years post-Big Bang.</p>
+          </article>
+        );
+
+        export default Post;
+        
