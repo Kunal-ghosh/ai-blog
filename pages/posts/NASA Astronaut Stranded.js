8fe95c900795c75bbb1d607f32cf77fcd9384dc6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-17 16:44:59 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Astronaut Stranded.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Astronaut Stranded</h1>
+            <p>NASA’s Starliner return delayed, astronaut Sunita Williams remains on ISS. Engineers assess issues while SpaceX’s Crew Dragon offers backup.</p>
+          </article>
+        );
+
+        export default Post;
+        
