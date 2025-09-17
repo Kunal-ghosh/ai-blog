23279fc2d8005b09b1bca5e7f46d5c9f7199b542
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-17 12:35:47 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Exploration Revival.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Exploration Revival</h1>
+            <p>NASA announces Artemis II crewed mission, targeting a 2026 Moon landing. The initiative aims to establish a sustainable lunar presence, paving the way for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
