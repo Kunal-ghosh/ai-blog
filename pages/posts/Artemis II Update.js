ec47c68bb782c40663bd04e5737ebb8350e572bf
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-17 03:26:57 +0000

    Add auto-generated post

--- pages/posts/Artemis II Update.js
+++ pages/posts/Artemis II Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Update</h1>
-            <p>NASA announces Artemis II crew finalizing training for 2025 lunar orbit mission, aiming to pave the way for sustained human presence on the Moon and future Mars exploration. The mission marks humanity’s first return to lunar space since 1972.</p>
+            <p>NASA announces Artemis II crew, readies 2024 lunar flyby mission.</p>
           </article>
         );
 
