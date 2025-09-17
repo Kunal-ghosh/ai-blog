49579e2a6a58162c162971d1162b3aeedb12ae1b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-17 17:14:28 +0000

    Add auto-generated post

---
+++ pages/posts/Olympic Torch Journey.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Olympic Torch Journey</h1>
+            <p>The Olympic flame arrived in Paris after a 12,000 km relay through French territories, ahead of the 2024 Summer Games. Final preparations intensify as athletes worldwide gear up for the July 26 Opening Ceremony.</p>
+          </article>
+        );
+
+        export default Post;
+        
