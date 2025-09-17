05bdc2a17f129802da96f46c92f5e573eefcd54a
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-17 03:45:47 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Water Exploration.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Water Exploration</h1>
+            <p>NASA’s VIPER rover, set to launch in 2024, will map lunar ice near the Moon’s south pole—a critical step for future crewed missions. The mission aims to locate resources for sustaining long-term human presence in space.</p>
+          </article>
+        );
+
+        export default Post;
+        
