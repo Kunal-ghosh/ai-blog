011e33153b0a83ebc04b81057457b18a4bb1e210
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-17 04:41:16 +0000

    Add auto-generated post

--- pages/posts/untitled.js
+++ pages/posts/untitled.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>untitled</h1>
-            <p></p>
+            <p>World leaders at COP28 concluded with a landmark deal to transition from fossil fuels, marking a historic step in climate action. The summit also emphasized funding for developing nations to combat escalating climate crises.</p>
           </article>
         );
 
