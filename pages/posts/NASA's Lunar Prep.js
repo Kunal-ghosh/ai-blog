e7f603dc392b5c99ab662c2ba950392e402181ae
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-17 07:16:48 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Lunar Prep.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Lunar Prep</h1>
+            <p>NASA announces Artemis II crew, targeting 2024 for a manned Moon orbit mission. The diverse team will test systems for future lunar exploration and Mars ambitions.</p>
+          </article>
+        );
+
+        export default Post;
+        
