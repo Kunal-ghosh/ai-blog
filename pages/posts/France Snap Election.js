4011d4f0755157b18a6b6e7caaa352c3fd3e2982
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 06:47:56 +0000

    Add auto-generated post

---
+++ pages/posts/France Snap Election.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>France Snap Election</h1>
+            <p>The first round of France's parliamentary elections sees the far-right National Rally leading, sparking debates on Europe's political future. Final voting on July 7 will determine the nation's shift amid rising ideological divides.</p>
+          </article>
+        );
+
+        export default Post;
+        
