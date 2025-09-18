b869d23b77fab3d3f291ef68ff18c92c95cce54a
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 10:17:37 +0000

    Add auto-generated post

---
+++ pages/posts/Icelandic Volcanic Eruption.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Icelandic Volcanic Eruption</h1>
+            <p>A recent eruption in Iceland's Reykjanes Peninsula forced evacuations and disrupted air travel. Authorities are monitoring lava flows and gas emissions as seismic activity continues.</p>
+          </article>
+        );
+
+        export default Post;
+        
