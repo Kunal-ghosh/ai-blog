6c371b08f36ef1149ba3727178f02285783b6d7d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 08:45:04 +0000

    Add auto-generated post

--- pages/posts/Artemis II Crew.js
+++ pages/posts/Artemis II Crew.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Crew</h1>
-            <p>NASA reveals the four astronauts selected for the 2024 Artemis II mission, marking humanity's first lunar flyby in 50 years. The diverse crew aims to pave the way for future Mars exploration and sustained Moon presence.</p>
+            <p>NASA unveiled the four astronauts chosen for the Artemis II mission, humanity’s first lunar flyby in over 50 years. The 2024 mission will test systems for future crewed Mars expeditions.</p>
           </article>
         );
 
