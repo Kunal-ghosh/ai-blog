0ba3960d3b5d77731700abede9aa4eb1ceb89534
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 13:43:21 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Discovery Breakthrough.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Discovery Breakthrough</h1>
+            <p>NASA's Perseverance rover uncovered organic molecules in Martian soil, hinting at potential ancient microbial life. This finding fuels hope for answering the age-old question: Are we alone in the universe?</p>
+          </article>
+        );
+
+        export default Post;
+        
