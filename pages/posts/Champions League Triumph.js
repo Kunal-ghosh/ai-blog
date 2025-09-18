936635ab672e87666e33e679f6ee9e8988899e65
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 17:39:16 +0000

    Add auto-generated post

---
+++ pages/posts/Champions League Triumph.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Champions League Triumph</h1>
+            <p>Real Madrid clinched their 15th UEFA Champions League title, defeating Borussia Dortmund 2-0 in a thrilling final at Wembley Stadium on June 1, 2024, cementing their dominance in European football history.</p>
+          </article>
+        );
+
+        export default Post;
+        
