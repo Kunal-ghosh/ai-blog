04d2cf0f0d201642b02947713f93eeaf6bae8525
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 01:15:56 +0000

    Add auto-generated post

---
+++ pages/posts/Euro 2024 Semifinals.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Euro 2024 Semifinals</h1>
+            <p>Spain defeated France 2-1 in a thrilling UEFA Euro 2024 semifinal, with 16-year-old Lamine Yamal scoring a record-breaking goal. France’s tournament exit sparks debates over Mbappé’s form, while Spain advances to Sunday’s final.</p>
+          </article>
+        );
+
+        export default Post;
+        
