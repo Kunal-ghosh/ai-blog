c1e8662517eb4fcbea5587f244f0c18ff8b7cc04
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 08:22:18 +0000

    Add auto-generated post

---
+++ pages/posts/Climate Protest Disruption.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Protest Disruption</h1>
+            <p>Activists targeted Botticelli’s *Primavera* at Venice Biennale, smashing its protective glass to protest fossil fuels. The stunt sparked global debates on balancing activism’s urgency with cultural heritage risks.</p>
+          </article>
+        );
+
+        export default Post;
+        
