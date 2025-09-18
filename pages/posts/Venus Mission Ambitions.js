b0620b36f82e973de78240047317f9dc8ebd5564
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 04:20:17 +0000

    Add auto-generated post

---
+++ pages/posts/Venus Mission Ambitions.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Venus Mission Ambitions</h1>
+            <p>Rocket Lab announces plans for a private mission to Venus by 2030, aiming to study its atmosphere for potential signs of microbial life using the Electron rocket. The mission, developed with MIT scientists, seeks to unravel mysteries behind the planet’s detected phosphine gas.</p>
+          </article>
+        );
+
+        export default Post;
+        
