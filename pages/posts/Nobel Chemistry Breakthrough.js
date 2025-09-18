91269467a0a09c879375df5e0110371040e4c522
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-18 18:25:25 +0000

    Add auto-generated post

---
+++ pages/posts/Nobel Chemistry Breakthrough.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Nobel Chemistry Breakthrough</h1>
+            <p>Three scientists awarded the 2023 Nobel Prize in Chemistry for pioneering quantum dots, nanoparticles revolutionizing tech and medicine. Their discovery enables advancements in TV screens and cancer treatments.</p>
+          </article>
+        );
+
+        export default Post;
+        
