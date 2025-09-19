54197bf987b4b19b1dc33199a34993f68a35ac18
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-19 07:37:04 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Climate Satellite.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Climate Satellite</h1>
+            <p>NASA launched PACE, a climate satellite, on February 8, 2024, to study ocean health and atmospheric particles. The mission aims to advance understanding of climate change impacts on marine ecosystems and global weather patterns.</p>
+          </article>
+        );
+
+        export default Post;
+        
