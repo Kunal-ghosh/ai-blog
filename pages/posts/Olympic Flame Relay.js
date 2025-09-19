0cfc4aa865abec79f32b622ae60f117a13ce9371
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-19 08:22:36 +0000

    Add auto-generated post

---
+++ pages/posts/Olympic Flame Relay.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Olympic Flame Relay</h1>
+            <p>The Paris 2024 Summer Olympics flame embarked on a global relay, emphasizing sustainability and unity. The journey began in Marseille, France, with plans to traverse 65 territories, spotlighting climate action and cultural exchange.</p>
+          </article>
+        );
+
+        export default Post;
+        
