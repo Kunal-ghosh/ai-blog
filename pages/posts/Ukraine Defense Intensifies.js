7a00f308117e03d193d5f4117f6011da854f833f
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-19 02:45:34 +0000

    Add auto-generated post

---
+++ pages/posts/Ukraine Defense Intensifies.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Ukraine Defense Intensifies</h1>
+            <p>Russian forces launched a renewed offensive in northeastern Ukraine, targeting Kharkiv region. Ukraine's military reports heavy fighting, with international allies pledging accelerated weapon shipments to bolster defense.</p>
+          </article>
+        );
+
+        export default Post;
+        
