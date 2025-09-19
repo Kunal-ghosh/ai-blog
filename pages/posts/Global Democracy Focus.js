f8bed0b76e7d8a182494afced885edbf9bd0da4d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-19 06:48:29 +0000

    Add auto-generated post

---
+++ pages/posts/Global Democracy Focus.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Global Democracy Focus</h1>
+            <p>World leaders and activists convened at the 2024 Oslo Freedom Forum in Taiwan, emphasizing challenges to global democracy and advocating for tech-driven solutions to counter authoritarianism. The event highlighted Taiwan’s role as a beacon of democratic resilience amid rising geopolitical tensions.</p>
+          </article>
+        );
+
+        export default Post;
+        
