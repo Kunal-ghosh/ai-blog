efabf37798ca6a7958e362f9ec2b89971eef0d10
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-19 09:18:56 +0000

    Add auto-generated post

---
+++ pages/posts/Hawaii Wildfires Aftermath.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Hawaii Wildfires Aftermath</h1>
+            <p>Devastating wildfires in Hawaii, fueled by drought and high winds, have destroyed homes and displaced thousands. Officials attribute the cause to downed power lines, sparking urgent climate resilience debates globally.</p>
+          </article>
+        );
+
+        export default Post;
+        
