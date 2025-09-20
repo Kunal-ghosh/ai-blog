313b1c64ae72297e15e34a25cc6545a48dc2686c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-20 11:37:10 +0000

    Add auto-generated post

---
+++ pages/posts/Solar Eclipse 2024.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Solar Eclipse 2024</h1>
+            <p>Millions witnessed the total solar eclipse on April 8, 2024, as it swept across North America, from Mexico to Canada. The rare celestial event drew crowds outdoors, with scientists highlighting its unique atmospheric and wildlife behavioral effects.</p>
+          </article>
+        );
+
+        export default Post;
+        
