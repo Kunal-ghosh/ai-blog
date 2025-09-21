f2460564560336d59c2d692a396804283fe3dcd4
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-21 15:15:03 +0000

    Add auto-generated post

---
+++ pages/posts/Iceland Volcano Erupts.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Iceland Volcano Erupts</h1>
+            <p>A volcanic eruption on Iceland's Reykjanes Peninsula triggered evacuations and halted tourism, spewing lava fountains and ash clouds. Scientists warn of prolonged activity, drawing global attention to the region's geologic volatility.</p>
+          </article>
+        );
+
+        export default Post;
+        
