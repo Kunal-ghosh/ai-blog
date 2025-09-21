8bf0cbf607655b2e47673d3f051069f1378f700d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-21 14:37:54 +0000

    Add auto-generated post

---
+++ pages/posts/EU Climate Package.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>EU Climate Package</h1>
+            <p>The European Union recently approved a landmark climate package, setting binding emissions cuts of 62% by 2030 and accelerating green energy investments. The policy aims to achieve carbon neutrality by 2050, amid debates over economic impacts and industry compliance.</p>
+          </article>
+        );
+
+        export default Post;
+        
