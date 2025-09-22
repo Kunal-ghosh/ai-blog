84ddfc06c4f12495d8dd5757f083a487f298104c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-22 21:35:48 +0000

    Add auto-generated post

---
+++ pages/posts/EU Climate Progress.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>EU Climate Progress</h1>
+            <p>The European Union recently approved the Nature Restoration Law, targeting ecosystem rehabilitation by 2030. The landmark legislation mandates member states to restore 20% of degraded land and sea areas, aiming to boost biodiversity.</p>
+          </article>
+        );
+
+        export default Post;
+        
