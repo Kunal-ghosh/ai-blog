f13f991ac37ef1e89d14a0d7a96b5996d68e091d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-23 23:36:29 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Sample Mission.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Sample Mission</h1>
+            <p>NASA and SpaceX collaborate to expedite Mars rock return, overcoming budget constraints and technical hurdles. The mission aims to deliver pristine samples by 2030, potentially unlocking secrets of Martian history and ancient life.</p>
+          </article>
+        );
+
+        export default Post;
+        
