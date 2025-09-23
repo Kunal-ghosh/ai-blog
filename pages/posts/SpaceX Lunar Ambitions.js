5e97cce7acde8e5a2f6503f8f2dad4d209603b6f
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-23 23:14:53 +0000

    Add auto-generated post

---
+++ pages/posts/SpaceX Lunar Ambitions.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>SpaceX Lunar Ambitions</h1>
+            <p>SpaceX completes Starship's first orbital test, achieving successful liftoff and splashdown. The milestone advances NASA's Artemis program for future lunar missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
