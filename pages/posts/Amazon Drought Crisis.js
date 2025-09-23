9158cba0cf265f2ddbf66ab31b039191b91af648
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-23 21:14:55 +0000

    Add auto-generated post

---
+++ pages/posts/Amazon Drought Crisis.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Amazon Drought Crisis</h1>
+            <p>Severe drought in the Amazon rainforest sparks global climate concerns, displacing wildlife and threatening indigenous communities. Record-low river levels disrupt transport and amplify fears of long-term ecological damage.</p>
+          </article>
+        );
+
+        export default Post;
+        
