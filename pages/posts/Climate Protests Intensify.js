e5bf49658f54552d09a37b4f870c315cea56384d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-24 07:38:37 +0000

    Add auto-generated post

---
+++ pages/posts/Climate Protests Intensify.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Protests Intensify</h1>
+            <p>Activists from the group Just Stop Oil vandalized Stonehenge with orange powder, demanding urgent climate action. The incident sparks global debate over protest methods and environmental priorities.</p>
+          </article>
+        );
+
+        export default Post;
+        
