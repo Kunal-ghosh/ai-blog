aa72b990b4761ca1494a2b0c0bb515db0b26130d
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-25 23:36:35 +0000

    Add auto-generated post

---
+++ pages/posts/Space Discovery Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Space Discovery Update</h1>
+            <p>The James Webb Space Telescope recently captured unprecedented high-resolution images of the Orion Nebula, revealing intricate details of stellar nurseries. Scientists say this breakthrough enhances understanding of star formation processes in distant galaxies.</p>
+          </article>
+        );
+
+        export default Post;
+        
