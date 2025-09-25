3636ca61535103efc7bd532e73ab33f1dbb869e5
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-25 07:38:13 +0000

    Add auto-generated post

--- pages/posts/untitled.js
+++ pages/posts/untitled.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>untitled</h1>
-            <p><p>Recent clashes escalate along the Israel-Lebanon border, raising fears of broader regional conflict. UN urges de-escalation amid exchanges of fire between Hezbollah and Israeli forces.<</p>
+            <p>untitled</p>
           </article>
         );
 
