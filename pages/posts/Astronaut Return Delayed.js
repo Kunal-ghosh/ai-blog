210ac93a474f8ebb378e06fc5aab509892364b70
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-26 19:36:56 +0000

    Add auto-generated post

---
+++ pages/posts/Astronaut Return Delayed.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Astronaut Return Delayed</h1>
+            <p>A NASA astronaut's return from the International Space Station was postponed due to a technical issue with the Boeing Starliner spacecraft. Engineers are assessing repairs while the crew remains safe aboard the station.</p>
+          </article>
+        );
+
+        export default Post;
+        
