107e4b5b9a4e32cae2d1bde89c011b264ec829d6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-26 12:36:22 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Landing Failure.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Landing Failure</h1>
+            <p>Israel's Beresheet 2 lunar lander crashed during its first test launch, delaying the nation's moon exploration ambitions. NASA and international partners are analyzing data to address technical issues ahead of future missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
