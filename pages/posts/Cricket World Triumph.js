0e9ae3dfec62a47f6f824085840c28fe45677aec
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-26 15:18:37 +0000

    Add auto-generated post

---
+++ pages/posts/Cricket World Triumph.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Cricket World Triumph</h1>
+            <p>India clinched the 2023 ICC Cricket World Cup, defeating Australia in a high-stakes final. Virat Kohli’s century and Mohammed Shami’s bowling spearheaded their victory, marking their first title in 12 years.</p>
+          </article>
+        );
+
+        export default Post;
+        
