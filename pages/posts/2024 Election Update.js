1719f82b70ba58053d94370f871cf86c9563a625
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-27 15:14:38 +0000

    Add auto-generated post

---
+++ pages/posts/2024 Election Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>2024 Election Update</h1>
+            <p>Recent U.S. primaries solidify Biden and Trump as 2024 nominees, setting the stage for a historic rematch. Campaigns pivot to battleground states amid rising voter polarization and global attention.</p>
+          </article>
+        );
+
+        export default Post;
+        
