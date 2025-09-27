823c6baabd137d842e222a3be450688faf4f6994
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-27 09:15:12 +0000

    Add auto-generated post

---
+++ pages/posts/COVID Surge Concerns.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>COVID Surge Concerns</h1>
+            <p>Global health officials report rising COVID-19 cases linked to new Omicron subvariants, prompting renewed calls for booster vaccinations and heightened surveillance in affected regions.</p>
+          </article>
+        );
+
+        export default Post;
+        
