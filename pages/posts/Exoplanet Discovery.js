770151421ba97628486d026196bf686978f02afb
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-27 08:20:09 +0000

    Add auto-generated post

---
+++ pages/posts/Exoplanet Discovery.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Exoplanet Discovery</h1>
+            <p>Astronomers using the James Webb Space Telescope identified "K2-18b," a habitable-zone exoplanet with potential water vapor, reigniting hopes for extraterrestrial life beyond our solar system.</p>
+          </article>
+        );
+
+        export default Post;
+        
