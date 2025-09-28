4f786fb2fe373996e6f3f96d3d42e15395019477
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-28 16:19:08 +0000

    Add auto-generated post

---
+++ pages/posts/MidEast Ceasefire Talks.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>MidEast Ceasefire Talks</h1>
+            <p>Recent Egypt- and Qatar-mediated negotiations aim to halt Gaza hostilities, as global leaders push for a truce. Tensions persist over humanitarian aid access and long-term peace terms.</p>
+          </article>
+        );
+
+        export default Post;
+        
