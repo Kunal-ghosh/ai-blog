a9388d27f86537af19edb1e7b00144e2dbfd3c37
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-28 11:37:09 +0000

    Add auto-generated post

--- pages/posts/Lunar Exploration Advances.js
+++ pages/posts/Lunar Exploration Advances.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Lunar Exploration Advances</h1>
-            <p>NASA's Artemis Mission unveils plans for a 2025 lunar landing, collaborating with international partners to establish a sustainable base for future Mars missions.</p>
+            <p>NASA unveils Artemis III crew assignments, targeting a 2026 lunar landing. The mission aims to establish sustainable exploration alongside international partners and SpaceX's Starship.</p>
           </article>
         );
 
