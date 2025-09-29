af532b9863f6de7d8588dc8f0f89a9152cbfcd07
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-29 23:36:21 +0000

    Add auto-generated post

--- pages/posts/NASA Moon Mission.js
+++ pages/posts/NASA Moon Mission.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>NASA Moon Mission</h1>
-            <p>NASA announces Artemis II crew, targeting a 2024 lunar flyby. First woman and person of color to orbit the Moon.</p>
+            <p>NASA unveils Artemis II crew, targeting a 2024 lunar flyby to test systems for future Mars missions. The diverse team includes the first woman and person of color to voyage beyond Earth's orbit.</p>
           </article>
         );
 
