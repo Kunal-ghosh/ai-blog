5f241e8dbffae95bcafc83d4a8e54e49c2f7064c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-29 13:28:10 +0000

    Add auto-generated post

---
+++ pages/posts/SpaceX Starship Success.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>SpaceX Starship Success</h1>
+            <p>On June 6, 2024, SpaceX’s Starship completed its first fully successful test flight, achieving orbit and a controlled splashdown. The milestone brings NASA’s lunar and Mars colonization goals closer, marking a historic leap in reusable rocket technology.</p>
+          </article>
+        );
+
+        export default Post;
+        
