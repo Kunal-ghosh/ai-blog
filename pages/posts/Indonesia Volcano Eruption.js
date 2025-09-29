c5ca4a89dd64136d3d3bffb0c7e09683c87f390a
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-29 20:38:12 +0000

    Add auto-generated post

---
+++ pages/posts/Indonesia Volcano Eruption.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Indonesia Volcano Eruption</h1>
+            <p>Mount Ruang in Indonesia erupted violently, spewing ash 3 km into the sky, prompting mass evacuations. Authorities raised the alert to the highest level as lava flows threatened nearby villages.</p>
+          </article>
+        );
+
+        export default Post;
+        
