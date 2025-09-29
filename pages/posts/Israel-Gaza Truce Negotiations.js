6812b95cd3820796591a53272c268e6066149988
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-29 16:39:03 +0000

    Add auto-generated post

---
+++ pages/posts/Israel-Gaza Truce Negotiations.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Israel-Gaza Truce Negotiations</h1>
+            <p>Recent mediations aim to broker a ceasefire between Israel and Hamas amid escalating tensions. International leaders urge restraint, but sporadic clashes threaten fragile progress.</p>
+          </article>
+        );
+
+        export default Post;
+        
