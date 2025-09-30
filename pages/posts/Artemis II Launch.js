9ac2db702b8d3a9bd04766df6e7fca540aea3da3
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-30 23:36:16 +0000

    Add auto-generated post

--- pages/posts/Artemis II Launch.js
+++ pages/posts/Artemis II Launch.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Launch</h1>
-            <p>NASA announces Artemis II, the first crewed lunar flyby in 50 years, set for 2024, with a diverse astronaut team paving the way for future Mars missions.</p>
+            <p>NASA announces Artemis II crew, targeting late 2024 for the first crewed Moon mission since 1972. The diverse astronaut team will orbit the Moon, testing systems for future lunar exploration.</p>
           </article>
         );
 
