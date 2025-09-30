f2049a9ee8da8e2bcae790a5e8dc17ef6f5e6470
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-09-30 19:37:23 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Lunar Return.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Lunar Return</h1>
+            <p>NASA's Artemis II mission advances, targeting a 2025 manned Moon orbit. The mission aims to test spacecraft systems and pave the way for sustainable lunar exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
