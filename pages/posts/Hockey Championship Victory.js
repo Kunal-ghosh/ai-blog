60de0bbe6e838293f8924ce54d8c5f9853763d81
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-01 09:40:56 +0000

    Add auto-generated post

---
+++ pages/posts/Hockey Championship Victory.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Hockey Championship Victory</h1>
+            <p>Canada clinched the 2024 IIHF World Championship gold, defeating Sweden 4-3 in a thrilling overtime finale. This marks their 28th title, solidifying their dominance in international ice hockey.</p>
+          </article>
+        );
+
+        export default Post;
+        
