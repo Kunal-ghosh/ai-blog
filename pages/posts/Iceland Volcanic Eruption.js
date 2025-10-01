425a455bf996fe2fd249147ea64403df369316b4
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-01 11:15:09 +0000

    Add auto-generated post

--- pages/posts/Iceland Volcanic Eruption.js
+++ pages/posts/Iceland Volcanic Eruption.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Iceland Volcanic Eruption</h1>
-            <p>A recent volcanic eruption in Iceland's Reykjanes Peninsula spewed lava and ash, disrupting air travel and prompting evacuations. Authorities are monitoring gas emissions and seismic activity as experts warn of prolonged geological instability.</p>
+            <p>A recent eruption of Iceland's Fagradalsfjall volcano captivated global attention, spewing lava flows and creating aurora-like illuminated skies. While authorities warned of gas hazards, tourists flocked to witness the rare geological spectacle.</p>
           </article>
         );
 
