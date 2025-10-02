b2185a185b6b7defea3d04973d75460601c1b4c5
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-02 18:24:34 +0000

    Add auto-generated post

--- pages/posts/untitled.js
+++ pages/posts/untitled.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>untitled</h1>
-            <p>Historic rainfall struck the UAE in April 2024, submerging Dubai streets and disrupting flights, as desert cities grappled with unprecedented flooding. Rescue operations intensified while climate experts linked the deluge to global warming patterns.</p>
+            <p>untitled</p>
           </article>
         );
 
