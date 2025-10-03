4ecde8140aaa56b72f37bac1162dd78e4768c5ae
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-03 15:17:28 +0000

    Add auto-generated post

---
+++ pages/posts/Global Climate Protests.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Global Climate Protests</h1>
+            <p>Millions worldwide rallied for urgent climate action, coinciding with COP28. Governments face pressure to accelerate emission cuts amid record heatwaves and ecological crises.</p>
+          </article>
+        );
+
+        export default Post;
+        
