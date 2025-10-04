2fdfb885a9c58f62212e66879a44783621b33c1c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-04 13:37:24 +0000

    Add auto-generated post

---
+++ pages/posts/Paris 2024 Mascot.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Paris 2024 Mascot</h1>
+            <p>The 2024 Paris Olympics unveiled "Phryges," a mascot duo inspired by the revolutionary Phrygian帽, symbolizing liberty and inclusivity. Fans worldwide can now purchase themed merchandise ahead of the Games’ July 26 opening ceremony.</p>
+          </article>
+        );
+
+        export default Post;
+        
