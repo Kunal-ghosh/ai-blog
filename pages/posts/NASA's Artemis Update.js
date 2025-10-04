45bd6557a2265cc253176530e4f3be5a8fe89228
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-04 02:35:43 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Artemis Update.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Artemis Update</h1>
+            <p>NASA unveils plans for Artemis II, targeting a 2026 crewed lunar landing. The mission aims to establish sustainable exploration, testing technologies for future Mars missions.</p>
+          </article>
+        );
+
+        export default Post;
+        
