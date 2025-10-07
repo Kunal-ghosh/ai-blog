de74897a0bd30eacadf753ff00a73d33fc02660e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-07 03:49:00 +0000

    Add auto-generated post

---
+++ pages/posts/Japan's Lunar Triumph.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Japan's Lunar Triumph</h1>
+            <p>Japan's SLIM spacecraft made a historic moon landing on January 19, becoming the fifth nation to achieve the feat. Despite solar panel issues, the mission showcases advancements in precision lunar exploration technology.</p>
+          </article>
+        );
+
+        export default Post;
+        
