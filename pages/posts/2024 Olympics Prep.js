e554734ca9da64ebf67d6e748aa1766cba269674
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-07 11:37:09 +0000

    Add auto-generated post

---
+++ pages/posts/2024 Olympics Prep.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>2024 Olympics Prep</h1>
+            <p>Paris intensifies security and infrastructure checks ahead of the 2024 Summer Olympics, prioritizing safety and sustainability. Meanwhile, athletes worldwide arrive for final training amid global anticipation.</p>
+          </article>
+        );
+
+        export default Post;
+        
