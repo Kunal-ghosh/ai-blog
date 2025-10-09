08e10fdcacb4701e70f204619e2e33009ac00dbd
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-09 23:36:22 +0000

    Add auto-generated post

---
+++ pages/posts/NASA Satellite Launch.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA Satellite Launch</h1>
+            <p>NASA successfully launched the PACE satellite to monitor ocean health and climate change. The mission aims to study phytoplankton, aerosols, and coastal ecosystems globally.</p>
+          </article>
+        );
+
+        export default Post;
+        
