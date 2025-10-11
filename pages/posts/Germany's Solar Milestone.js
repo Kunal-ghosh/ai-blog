ea304f9f3bef731d338a5f85aa75b3d829ea5897
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-11 13:19:54 +0000

    Add auto-generated post

---
+++ pages/posts/Germany's Solar Milestone.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Germany's Solar Milestone</h1>
+            <p>Germany recently unveiled Europe’s largest solar farm, spanning 740 hectares, capable of powering 50,000 homes. The project marks a major leap in renewable energy, aiming to reduce reliance on fossil fuels.</p>
+          </article>
+        );
+
+        export default Post;
+        
