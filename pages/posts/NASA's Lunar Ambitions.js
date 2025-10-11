35721a234d002554c7e75990233ed21a22dbabcd
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-11 06:44:48 +0000

    Add auto-generated post

---
+++ pages/posts/NASA's Lunar Ambitions.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>NASA's Lunar Ambitions</h1>
+            <p>NASA unveils plans for Artemis III, aiming to land astronauts near the Moon's south pole by 2026. The mission seeks to establish a sustainable human presence and test technologies for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
