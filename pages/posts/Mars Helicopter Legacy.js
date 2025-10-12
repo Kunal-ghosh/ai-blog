8a7e7dcef73bc86a3e51d92cc870ce9baa82a628
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-12 17:13:38 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Helicopter Legacy.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Helicopter Legacy</h1>
+            <p>NASA's Ingenuity completes 72 flights on Mars, surpassing all expectations. The rotorcraft’s mission paves the way for future aerial exploration of the Red Planet.</p>
+          </article>
+        );
+
+        export default Post;
+        
