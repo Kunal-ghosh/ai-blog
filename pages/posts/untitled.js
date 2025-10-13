47024f197a21918413462de0f632eb27b1fbd4ed
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-13 08:49:04 +0000

    Add auto-generated post

--- pages/posts/untitled.js
+++ pages/posts/untitled.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>untitled</h1>
-            <p>untitled</p>
+            <p>NASA unveiled the Artemis II crew on April 3, 2023, featuring the first woman, person of color, and Canadian for a lunar orbit mission. The 2024 launch aims to pave the way for future Mars exploration.</p>
           </article>
         );
 
