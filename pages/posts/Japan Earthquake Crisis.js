27aaa309cb9b94973c5efddb075a14d6888c831b
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-16 10:43:44 +0000

    Add auto-generated post

---
+++ pages/posts/Japan Earthquake Crisis.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Japan Earthquake Crisis</h1>
+            <p>A 7.5 magnitude earthquake struck Ishikawa, Japan, triggering tsunami warnings and evacuations. Rescue operations intensify as aftershocks and structural damages hinder relief efforts.</p>
+          </article>
+        );
+
+        export default Post;
+        
