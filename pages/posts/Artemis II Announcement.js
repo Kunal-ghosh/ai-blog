cb6647f481676d077ff761162f983fd7c4cc49fc
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-16 19:37:42 +0000

    Add auto-generated post

--- pages/posts/Artemis II Announcement.js
+++ pages/posts/Artemis II Announcement.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis II Announcement</h1>
-            <p>NASA unveils crew for Artemis II, marking the first manned Moon mission in 50 years. The diverse team of astronauts will orbit the Moon in late 2024, advancing lunar exploration goals.</p>
+            <p>NASA unveils crew for 2025 Artemis II mission, marking humanity's first lunar flyby in 50 years. The mission paves the way for future Mars exploration.</p>
           </article>
         );
 
