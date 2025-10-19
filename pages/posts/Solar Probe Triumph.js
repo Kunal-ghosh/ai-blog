af0e3f7f086ba1dfc335b49a01ffbabfd90096ca
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-19 08:20:27 +0000

    Add auto-generated post

---
+++ pages/posts/Solar Probe Triumph.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Solar Probe Triumph</h1>
+            <p>NASA's Parker Solar Probe recently shattered records, skimming 3.8 million miles from the Sun's surface at 394,736 mph—unlocking new solar mysteries</p>
+          </article>
+        );
+
+        export default Post;
+        
