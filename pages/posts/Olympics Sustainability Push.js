20baeb3e7bcfdac572d37386084bc36d54a294d6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-21 19:37:49 +0000

    Add auto-generated post

---
+++ pages/posts/Olympics Sustainability Push.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Olympics Sustainability Push</h1>
+            <p>Paris 2024 unveils solar-powered venues and stringent eco-policies, aiming for the greenest Games. Athletes’ villages feature renewable energy and low-carbon transport, setting new climate benchmarks.</p>
+          </article>
+        );
+
+        export default Post;
+        
