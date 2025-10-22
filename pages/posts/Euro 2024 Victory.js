22d155b8b1b8c34671d548ac13e2b968cba869cb
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-22 02:54:32 +0000

    Add auto-generated post

---
+++ pages/posts/Euro 2024 Victory.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Euro 2024 Victory</h1>
+            <p>Spain clinched the Euro 2024 title with a thrilling 2-1 win over England, marking their record-breaking fourth European Championship triumph. Teen sensation Lamine Yamal and match-winner Mikel Oyarzabal electrified Berlin’s final, ending England’s hopes of ending a 58-year major trophy drought.</p>
+          </article>
+        );
+
+        export default Post;
+        
