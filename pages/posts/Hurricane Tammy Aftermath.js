0416b1dca57490c6dac4b084101a6cd6e1986d19
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-24 09:19:48 +0000

    Add auto-generated post

---
+++ pages/posts/Hurricane Tammy Aftermath.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Hurricane Tammy Aftermath</h1>
+            <p>Hurricane Tammy caused widespread flooding and evacuations in the Caribbean, disrupting infrastructure and displacing thousands. Authorities are coordinating relief efforts as the storm weakens, prioritizing aid to hardest-hit regions.</p>
+          </article>
+        );
+
+        export default Post;
+        
