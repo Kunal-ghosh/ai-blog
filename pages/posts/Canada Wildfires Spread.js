edffbded7064c8f1a5891e5351811e37dc4ec8df
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-25 05:39:08 +0000

    Add auto-generated post

---
+++ pages/posts/Canada Wildfires Spread.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Canada Wildfires Spread</h1>
+            <p>Smoke from intense wildfires in Canada has blanketed parts of the northeastern U.S., triggering air quality alerts. Officials urge vulnerable groups to limit outdoor activities as haze reaches hazardous levels.</p>
+          </article>
+        );
+
+        export default Post;
+        
