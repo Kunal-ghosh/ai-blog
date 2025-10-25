ab0c2f69b6c329236fb286b264b3eab65cdd60bc
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-25 21:36:43 +0000

    Add auto-generated post

---
+++ pages/posts/Global Rights Rally.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Global Rights Rally</h1>
+            <p>World leaders and activists convened at the Oslo Freedom Forum, addressing urgent human rights challenges. Discussions highlighted rising authoritarianism and tech's role in oppression, urging global solidarity for democratic reforms.</p>
+          </article>
+        );
+
+        export default Post;
+        
