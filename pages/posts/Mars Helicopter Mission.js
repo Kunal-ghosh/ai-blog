76d0b0de65a1aa4660586de7a47dce5bf2386a01
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-27 09:22:07 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Helicopter Mission.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Helicopter Mission</h1>
+            <p>NASA's Ingenuity helicopter, after 72 flights on Mars, ends its mission due to rotor damage. The tiny craft surpassed expectations, proving powered flight is possible on the Red Planet.</p>
+          </article>
+        );
+
+        export default Post;
+        
