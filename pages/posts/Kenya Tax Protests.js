32596ec625275447a948b8310b78b884998ad432
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-31 10:19:23 +0000

    Add auto-generated post

---
+++ pages/posts/Kenya Tax Protests.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Kenya Tax Protests</h1>
+            <p>Kenya withdrew its controversial finance bill following nationwide protests and violent clashes, which left over 20 dead. The move aims to ease public unrest amid economic pressures and rising living costs.</p>
+          </article>
+        );
+
+        export default Post;
+        
