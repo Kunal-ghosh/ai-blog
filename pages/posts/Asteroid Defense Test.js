06a4c1a50d4ac5c3cb5da54441d700a7569f4a65
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-10-31 11:36:45 +0000

    Add auto-generated post

---
+++ pages/posts/Asteroid Defense Test.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Asteroid Defense Test</h1>
+            <p>NASA and global agencies recently conducted a planetary defense simulation, testing responses to a hypothetical asteroid threat. The exercise aimed to refine strategies for detection, deflection, and international coordination in case of near-Earth object risks.</p>
+          </article>
+        );
+
+        export default Post;
+        
