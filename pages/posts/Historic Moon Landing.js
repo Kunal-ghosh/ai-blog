172a6a2076f6dbd658700cb500432ee777952e32
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-01 09:37:28 +0000

    Add auto-generated post

---
+++ pages/posts/Historic Moon Landing.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Historic Moon Landing</h1>
+            <p>Japan’s SLIM spacecraft successfully landed on the Moon, becoming the fifth nation to achieve a lunar touchdown. The mission faced power challenges but showcased advancements in precision landing technology.</p>
+          </article>
+        );
+
+        export default Post;
+        
