a55bb1996016f3a4e30c0779697016613ae6a983
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-01 09:16:58 +0000

    Add auto-generated post

---
+++ pages/posts/Climate Emergency Warning.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Climate Emergency Warning</h1>
+            <p>UN reports 2023 as Earth’s hottest year on record, urging immediate global action to curb emissions amid rising wildfires and extreme weather disasters.</p>
+          </article>
+        );
+
+        export default Post;
+        
