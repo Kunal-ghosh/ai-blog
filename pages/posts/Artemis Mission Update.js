5af435d16701102d589a733eecc16d3883e51872
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-01 11:37:18 +0000

    Add auto-generated post

--- pages/posts/Artemis Mission Update.js
+++ pages/posts/Artemis Mission Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis Mission Update</h1>
-            <p>NASA announces Artemis II crew for 2024 lunar flyby, marking humanity's first return to the Moon since 1972. The mission aims to pave the way for sustainable lunar exploration and future Mars expeditions.</p>
+            <p>NASA's Artemis II crew outlined plans for a 2024 lunar flyby, marking humanity's first return to the moon since Apollo. The mission aims to test spacecraft systems with an international crew, paving the way for future lunar exploration.</p>
           </article>
         );
 
