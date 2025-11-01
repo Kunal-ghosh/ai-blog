6727ad295851bddbcbbc2667d76b621c16b13762
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-01 02:55:12 +0000

    Add auto-generated post

---
+++ pages/posts/France Snap Elections.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>France Snap Elections</h1>
+            <p>President Macron dissolved France’s parliament, calling snap elections after a major setback in EU polls. The move sparks political uncertainty as far-right gains reshape the nation’s electoral landscape.</p>
+          </article>
+        );
+
+        export default Post;
+        
