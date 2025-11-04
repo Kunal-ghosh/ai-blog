884d8c29123dc75c67bf892df915721ea508a743
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-04 03:41:13 +0000

    Add auto-generated post

--- pages/posts/Artemis Lunar Update.js
+++ pages/posts/Artemis Lunar Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Artemis Lunar Update</h1>
-            <p>NASA announces Artemis III mission, targeting 2026 for the first crewed Moon landing since 1972, emphasizing international collaboration and lunar exploration sustainability.</p>
+            <p>NASA unveils revised Artemis III timeline, targeting 2026 for the first crewed lunar landing since 1972. The mission prioritizes sustainable exploration and international collaboration for future Mars missions.</p>
           </article>
         );
 
