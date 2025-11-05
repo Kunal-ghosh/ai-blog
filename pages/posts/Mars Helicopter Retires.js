7e95e87b654243d5293b3af56bf6e0c8b28b16be
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-05 11:16:23 +0000

    Add auto-generated post

---
+++ pages/posts/Mars Helicopter Retires.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mars Helicopter Retires</h1>
+            <p>NASA's Ingenuity helicopter, after 72 flights on Mars, officially ended its mission due to rotor damage. The tiny craft exceeded expectations, revolutionizing plans for future extraterrestrial exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
