6a65eb6737a66e5f7a933fdc5b90b3863961f51e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-05 21:17:28 +0000

    Add auto-generated post

---
+++ pages/posts/Paris 2024 Opening.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Paris 2024 Opening</h1>
+            <p>The Paris 2024 Olympics kicked off with a dazzling ceremony along the Seine River, featuring 10,500 athletes from 206 nations. Innovations in sustainability, like drone light shows and renewable energy, highlighted the event’s eco-conscious vision.</p>
+          </article>
+        );
+
+        export default Post;
+        
