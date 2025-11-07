b308c407032a64e846fe672d547f7dace15be39a
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-07 10:20:20 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Mission Advances.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Mission Advances</h1>
+            <p>NASA unveils Artemis II crew, targeting a 2024 lunar flyby to test systems for future manned Mars expeditions. The mission marks humanity's first return to the Moon in over 50 years.</p>
+          </article>
+        );
+
+        export default Post;
+        
