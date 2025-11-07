2000989e95641f8e84321dddf61280722f1286b6
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-07 18:25:49 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis I Return.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis I Return</h1>
+            <p>NASA's Orion capsule splashed down on December 11, 2022, completing the 25-day Artemis I mission. The uncrewed lunar journey paves the way for future astronaut moon landings.</p>
+          </article>
+        );
+
+        export default Post;
+        
