1b521830fa1e8f8b736813420888872130ac099e
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-10 05:42:03 +0000

    Add auto-generated post

---
+++ pages/posts/Stellar Nebula Discoveries.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Stellar Nebula Discoveries</h1>
+            <p>James Webb Telescope unveils unprecedented images of the Orion Nebula, revealing intricate details of star formation. Scientists hail the data as a breakthrough in understanding cosmic evolution.</p>
+          </article>
+        );
+
+        export default Post;
+        
