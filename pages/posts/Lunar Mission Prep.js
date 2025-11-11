4a6c96e62aec4010d8106c308a97f5fd5b0fc50a
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-11 08:24:37 +0000

    Add auto-generated post

---
+++ pages/posts/Lunar Mission Prep.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Lunar Mission Prep</h1>
+            <p>NASA's Artemis II crew begins intensive training for 2025 Moon orbit, marking humanity's first lunar mission in 50 years. The diverse team aims to pave the way for future Mars exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
