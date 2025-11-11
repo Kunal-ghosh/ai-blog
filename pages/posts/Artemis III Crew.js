517281abfef15bd75c20bcb3f8da890bd9860766
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-11 19:14:26 +0000

    Add auto-generated post

---
+++ pages/posts/Artemis III Crew.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Artemis III Crew</h1>
+            <p>NASA announced the Artemis III crew, targeting a 2025 lunar landing. The mission features the first woman and person of color to explore the Moon, marking a historic leap in space exploration.</p>
+          </article>
+        );
+
+        export default Post;
+        
