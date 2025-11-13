f9bf4f613848e8daf66976e92599ca5a35969a93
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-13 04:24:01 +0000

    Add auto-generated post

--- pages/posts/Lunar Mission Update.js
+++ pages/posts/Lunar Mission Update.js
@@ -4,7 +4,7 @@
         const Post = () => (
           <article style={{ padding: '2rem' }}>
             <h1>Lunar Mission Update</h1>
-            <p>NASA unveils Artemis II crew, targeting a 2024 lunar flyby. The diverse team includes the first woman and person of color to orbit the Moon, advancing deep-space exploration goals.</p>
+            <p>NASA's Artemis II crew prepares for a 2024 lunar orbit, marking humanity's return to the Moon. The mission paves the way for future Mars exploration, emphasizing global collaboration in space endeavors.</p>
           </article>
         );
 
