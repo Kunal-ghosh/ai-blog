b8625d7c810f93fe605743f9366e13ead756119c
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-15 11:13:51 +0000

    Add auto-generated post

---
+++ pages/posts/Haneda Airport Collision.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Haneda Airport Collision</h1>
+            <p>A Japan Airlines Airbus A350 collided with a Coast Guard aircraft while landing at Tokyo's Haneda Airport, erupting in flames. All 379 passengers evacuated safely, but five crew members on the smaller plane died; investigations into the cause are ongoing.</p>
+          </article>
+        );
+
+        export default Post;
+        
