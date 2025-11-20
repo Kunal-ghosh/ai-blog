040b48e8905fdaf471f4c8ed15cc92db7fa522ed
Kunal-ghosh <kunalghosh5135@gmail.com>
2025-11-20 13:47:26 +0000

    Add auto-generated post

---
+++ pages/posts/Mideast Truce Talks.js
@@ -0,0 +1,12 @@
+
+        import React from 'react';
+
+        const Post = () => (
+          <article style={{ padding: '2rem' }}>
+            <h1>Mideast Truce Talks</h1>
+            <p>Recent negotiations between Israel and Hamas, mediated by Egypt and Qatar, aim to secure a ceasefire and hostage exchange in Gaza. Talks face hurdles over lasting peace terms, as global pressure mounts to halt escalating civilian casualties.</p>
+          </article>
+        );
+
+        export default Post;
+        
